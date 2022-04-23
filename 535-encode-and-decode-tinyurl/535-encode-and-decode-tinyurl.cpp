0ff175adbf1e788664f735770967d13ca4aeeb9f
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-04-23 19:51:24 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (5.80%) - LeetHub

---
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        long_to_short[longUrl] = "http://tinyurl.com/" + to_string(hash_function(longUrl));
+        short_to_long[long_to_short[longUrl]] = longUrl;
+        return long_to_short[longUrl];
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return short_to_long[shortUrl];
+    }
+    
+private:
+    uint64_t hash_function(const string& str) {
+        uint64_t hash = 0x811c9dc5;
+        uint64_t prime = 0x1000193;
+
+        for(int i = 0; i < str.size(); ++i) {
+            uint8_t value = str[i];
+            hash = hash ^ value;
+            hash *= prime;
+        }
+
+        return hash;
+    }
+    
+    unordered_map<string, string> long_to_short;
+    unordered_map<string, string> short_to_long;
+};
