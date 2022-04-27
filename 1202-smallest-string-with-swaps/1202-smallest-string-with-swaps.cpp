a6c7e0cc4ee6729228c23f53c99399bc3476d8f7
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-04-28 00:16:21 +0530

    Time: 244 ms (54.16%), Space: 47.2 MB (96.57%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
@@ -1,51 +1,49 @@
 class Solution {
-    
-public:
-    void dfs(vector<vector<int>>& adjList, int x, unordered_set<int>& seen, vector<string>& sets, int seti, unordered_map<int, int>& parentSet, string& s) {
-        if(seen.find(x) != seen.end()) return;
-        seen.insert(x);
-        sets[seti].push_back(s[x]);
-        parentSet[x] = seti;
-        auto& children = adjList[x];
-        for(auto child : children) {
-            dfs(adjList, child, seen, sets, seti, parentSet, s);
-        }
-    }
-    
-    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
-        int n = s.length();
-        vector<vector<int>> adjList(n);
-        
-        for(auto& pair : pairs) {
-            adjList[pair[0]].push_back(pair[1]);
-            adjList[pair[1]].push_back(pair[0]);
-        }
-        
-        unordered_set<int> seen;
-        vector<string> sets;
-        unordered_map<int, int> parentSet;
-        int seti = 0;
-        
-        for(int i = 0; i < n; i++) {
-            if(seen.find(i) == seen.end()) {
-                string newSet = "";
-                sets.push_back(newSet);
-                dfs(adjList, i, seen, sets, seti, parentSet, s);
-                seti++;
-            }
-        }
-        int setSize = sets.size();
-        
-        vector<int> its(setSize);
-        for(auto& thing : sets) {
-            sort(thing.begin(), thing.end());
-        }
-        for(int i = 0; i < n; i++) {
-            int j = parentSet[i];      
-            s[i]  = sets[j][its[j]++];  
-        }
 
-        return s;
-        
+int find(int x,vector<int>& parent){
+    if(x==parent[x]){
+        return x;
     }
+    
+    return parent[x]=find(parent[x],parent);
+    
+}
+void Union(int x,int y,vector<int>& parent){
+    int px=find(x,parent);
+    int py=find(y,parent);
+    
+    if(px!=py){
+        parent[px]=py;
+    }
+}
+public:
+string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+
+    int n=s.size();
+    vector<int> parent(n);
+    
+    for(int i=0;i<n;i++){
+        parent[i]=i;
+    }
+    
+    for(int i=0;i<pairs.size();i++){
+        Union(pairs[i][0],pairs[i][1],parent);
+    }
+    
+    unordered_map<int,priority_queue<char,vector<char>,greater<char>>> mp;
+    
+    for(int i=0;i<n;i++){
+        mp[find(i,parent)].push(s[i]);
+    }
+    
+    string ans="";
+    for(int i=0;i<n;i++){
+        int pnode=find(i,parent);
+        char ch=mp[pnode].top();
+        ans.push_back(ch);
+        mp[pnode].pop();
+    }
+    
+    return ans;
+}
 };
