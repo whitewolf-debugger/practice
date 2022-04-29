832b8f0228eb6533cff8cae9928fd3b2250e40e9
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-04-29 23:09:56 +0530

    Time: 24 ms (85.26%), Space: 14.4 MB (17.74%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,30 +1,31 @@
 class Solution {
-    void dfs(vector<vector<int>>& graph,int x, int color,unordered_map<int,int>& colors,bool &ans){
+    void dfs(vector<vector<int>>& graph,int x,int color,unordered_map<int,int> &colors,bool &ans){
         if(!ans) return;
-        if(colors.find(x) != colors.end()){
-            if(colors[x] !=color){
+        if(colors.find(x)!=colors.end()){
+            if(colors[x]!=color){
                 ans = false;
             }
             return;
         }
         colors[x]=color;
-        auto& children = graph[x];
-        for(auto child : children){
-            dfs(graph,child,color^1,colors,ans);
+        auto children = graph[x];
+        for(auto node : children){
+            dfs(graph,node,color^1,colors,ans);
         }
-        return;
     }
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int n= graph.size();
-        unordered_map<int,int> colors;
         bool ans = true;
+        int n=graph.size();
+        unordered_map<int,int> colors;
         for(int i=0;i<n;i++){
             if(colors.find(i)==colors.end()){
                 dfs(graph,i,0,colors,ans);
-                if(!ans) return ans;
+                if(!ans){
+                    return false;
+                }
             }
         }
-        return ans;
+        return true;
     }
 };
