5f8e22a5c22ea8b28b01869bf416226c4313f213
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-05-02 11:27:00 +0530

    Time: 17 ms (35.77%), Space: 16.8 MB (7.31%) - LeetHub

---
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        vector<int> ans;
+        vector<int> eve;
+        vector<int> odd;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0){
+                eve.push_back(nums[i]);
+            }
+            else{
+                odd.push_back(nums[i]);
+            }
+        }
+        eve.insert(eve.end(),odd.begin(),odd.end());
+        return eve;
+    }
+};
