07bb495f1ddb84d24e44611780036971df939939
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-05-06 00:58:57 +0530

    Time: 2 ms (41.10%), Space: 5.9 MB (26.32%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     long int moves(int target, int maxDoubles){
+        //if target is less than 1 we return 0
         if(target<=1){
             return 0;
         }
+        //basecases if maxDouble is 0 then ans is target-1
         if(maxDoubles==0){
             return target-1;
         }
         if(target%2==0 && maxDoubles>0){
+            //if target is divisible by 2 then we use maxDoubles 
             return 1+moves(target/2,maxDoubles-1);
         }
         else{
+            //else we just decrese target
             return 1+moves(target-1,maxDoubles);
         }
     }
