7e6b36784f0f67e2b2ac49814cc92ca4532d4420
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-05-06 20:09:04 +0530

    Time: 16 ms (92.01%), Space: 9.9 MB (85.45%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -10,14 +10,18 @@ public:
                 auto top = stk.top();
                 char prev = top.first;
                 int currentstreak =top.second;
+                // if the stack is not empty the prev character and current are same
                 if(prev == c){
                     stk.pop();
+                    //pop the character from stack
                     int newstreak = currentstreak+1;
+                    //calculate new streak if it is less than k then push it in stack else we dont do anything
                     if(newstreak<k){
                         stk.push({c,newstreak});
                     }
                 }
                 else{
+                    //if current and prev element are not same push character and its streak as 1
                     stk.push({c,1});
                 }
             }
@@ -28,11 +32,19 @@ public:
             char c = top.first;
             stk.pop();
             int howmany = top.second;
+            //add that caracter number of times it was present 
             for(int i=0;i<howmany;i++){
+                //rebuild the ans string
                 ans+=c;
             }
         }
+        //reverse the ans since we are using stacks 
         reverse(ans.begin(),ans.end());
         return ans;
     }
 };
+/*
+    Keep a stack with the character and it's streak.
+    If the steak is equal to K, then remove it from the stack.
+    rebuild the string and reverse it to get the final answer.
+*/
