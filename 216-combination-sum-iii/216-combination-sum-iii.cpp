7ee85c618d1fb41fd0b4298497daa1fc11655a12
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-05-10 23:22:40 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (58.80%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -2,15 +2,17 @@ class Solution {
     void dfs(int num,int sum,int count,vector<int>& path,vector<vector<int>>& ans){
         if(sum == 0 && count ==0 ){
             ans.push_back(path);
-            return ;
+            return;
         }
         if(sum==0 || count ==0) return;
         if(num > 9) return;
+        //take it 
         if(sum >= num && count>0){
             path.push_back(num);
             dfs(num+1,sum-num,count-1,path,ans);
             path.pop_back();
         }
+        // leave it 
         dfs(num+1,sum,count,path,ans);
     }
 public:
