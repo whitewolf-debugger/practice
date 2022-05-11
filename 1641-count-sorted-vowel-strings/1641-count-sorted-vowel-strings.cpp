1a559dfface8ecb67ce9b50c5ae95a87f58b5b33
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-05-11 16:16:52 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (85.48%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,16 +1,24 @@
 class Solution {
-    int dfs(int n,int i){
-        if(n ==0){
-            return 1;
-        }
-        int ret =0;
-        for(int j = i ; j<5;j++){
-            ret += dfs(n-1,j);
-        }
-        return ret;
-    }
+    // int dfs(int n,int i){
+    //     if(n ==0){
+    //         return 1;
+    //     }
+    //     int ret =0;
+    //     for(int j = i;j<5;j++){
+    //         ret += dfs(n-1,j);
+    //     }
+    //     return ret;
+    // }
 public:
     int countVowelStrings(int n) {
-        return dfs(n,0);
+        
+        int sol = (n+4)*(n+3)*(n+2)*(n+1)/24;
+        return sol;
+        //return dfs(n,0);
     }
 };
+/*
+
+  9!/(5! 4!)
+  (n+4)*(n+3)*(n+2)*(n+1)/24
+*/
