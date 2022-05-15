d563f23dd49c61ba84ea6c29d0ad3c4ce92ecf66
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-05-15 22:48:03 +0530

    Time: 127 ms (54.83%), Space: 59.7 MB (76.50%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -1,32 +1,20 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-    void dfs(TreeNode* root, vector<int>& deep, int depth) {
-        if(!root) return;
-        if(depth >= deep.size()) {
-            deep.push_back(root->val);
-        } else {
-            deep[depth] += root->val;
-        }
-        
-        dfs(root->left, deep, depth+1);
-        dfs(root->right, deep, depth+1);
-        
-    }
 public:
+    long int ans = 0, tmp = -1;
+    void fun(TreeNode* root, int level){
+        if(!root)return;
+        if(!root->left && !root->right){
+            if(level > tmp){
+                tmp = level;
+                ans = root->val;
+            }
+            else if(level == tmp)ans += root->val;
+        } 
+        fun(root->left, level + 1);
+        fun(root->right, level + 1);
+    }
     int deepestLeavesSum(TreeNode* root) {
-        vector<int> deeps;
-        dfs(root, deeps, 0);
-        return deeps.back();
+        fun(root, 0);
+        return ans;
     }
 };
-
