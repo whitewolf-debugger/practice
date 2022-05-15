34f54476105d3d62fd2f933cf0a1935a49370231
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-05-15 22:55:27 +0530

    Time: 143 ms (35.47%), Space: 59.7 MB (76.50%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     long int ans = 0, tmp = -1;
-    void fun(TreeNode* root, int level){
-        //base casse 
+    void dfs(TreeNode* root, int level){
+        //base case if root == NULL terminate
         if(!root)return;
+        // if it is leaf node 
         if(!root->left && !root->right){
+            //if level is more than current max level stored in temp
             if(level > tmp){
+                //current max level = level
                 tmp = level;
+                //ans  = root->val value at that depth
                 ans = root->val;
             }
+            // if it is the final depth then add root ->val to the previous data at that level
             else if(level == tmp)ans += root->val;
         } 
-        fun(root->left, level + 1);
-        fun(root->right, level + 1);
+        //call at left  and increase level
+        dfs(root->left, level + 1);
+        //call at right and increase level
+        dfs(root->right, level + 1);
     }
     int deepestLeavesSum(TreeNode* root) {
-        fun(root, 0);
+        dfs(root, 0);
         return ans;
     }
 };
