92f5c7628f0850c75e375867018b97bb5a26de8b
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-05-21 19:57:00 +0530

    Time: 65 ms (90.62%), Space: 10.6 MB (79.56%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,23 +1,21 @@
+// Pure Recursion : TC : O(2^N)
 class Solution {
 public:
-    int memo[13][10001];
+     int dp[13][10001];
     // Unbounded KnapSack 
     int rec(vector<int> &coins, int n, int W){
+
         if(W == 0) return 0;
-        
+
         if(n == 0) return INT_MAX-1;
-        
-        // Memoization Stuff
-        if(memo[n][W] != -1) return memo[n][W];
-        
+        if(dp[n][W]!=-1) return dp[n][W];
         if(coins[n-1] <= W){
-            return memo[n][W] = min(1 + rec(coins, n, W-coins[n-1]), rec(coins, n-1, W));
+            return dp[n][W] = min(1 + rec(coins, n, W-coins[n-1]), rec(coins, n-1, W));
         }
-        
-        return memo[n][W] = rec(coins, n-1, W);
+        return dp[n][W] = rec(coins, n-1, W);
     }
     int coinChange(vector<int>& coins, int amount){
-        memset(memo, -1, sizeof(memo));
+        memset(dp, -1, sizeof(dp));
         int minCoins = rec(coins, coins.size(), amount);
         return minCoins == INT_MAX-1  ? -1 : minCoins;
     }
