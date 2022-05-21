1970c9eac931edd526ad9e88c280d3b0cc412772
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-05-22 00:09:14 +0530

    Time: 53 ms (5.09%), Space: 39.3 MB (6.32%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     void dfs(TreeNode* root,vector<vector<int>>& ans,vector<int> path,int target){
