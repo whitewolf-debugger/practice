def021f988bd01e5cca17e27dcb0546dce024502
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-05-22 10:15:33 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (82.67%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -8,6 +8,7 @@ public:
         for(int i=0;i<s.size();i++){
             left =i;
             right=i;
+            // OddPalSubseq counts total number of odd sized  palindrome subsequence 
             while(left>=0 && right<s.size() && s[left]==s[right]){
                 OddPalSubseq++;
                 left--;
@@ -15,12 +16,23 @@ public:
             }
             left =i;
             right = i+1;
+            //EvePalSubseq counts total number of even sized palindrom subsequnce
             while(left>=0 && right<s.size() && s[left]==s[right] ){
                 EvePalSubseq++;
                 left--;
                 right++;
             }
         }
+        //total number of even size palindrome subsequence  + total number of odd sized palindrome subsequence
         return OddPalSubseq+EvePalSubseq;
     }
 };
+/*
+     a      a       a       b
+            ↑
+     ←    center    →
+   left--          right++
+   
+   select a center and expand on both left and right
+        
+*/
