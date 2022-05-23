8677b1e43fc1f80908938856c2c0da9935bf18da
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-05-23 21:36:23 +0530

    Time: 17 ms (77.60%), Space: 14.2 MB (86.76%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,21 +10,25 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // base case if we reach posn where root is null return NULL
         if(root == NULL) return NULL;
+        //if we reach a point where we got left lCA or right lca first we return that root
         if(root->val == p->val || root->val ==q->val){
             return root;
         }
+        // get the leftLCA  and the rightLCA 
         TreeNode* leftLCA= lowestCommonAncestor(root->left,p,q);
         TreeNode* rightLCA = lowestCommonAncestor(root->right,p,q);
+        // if both of them are not null means the dta is present in both the sides of means root is the lca
         if( leftLCA!=NULL && rightLCA!=NULL){
             return root;
         }
+        // if one of them is LCA return that 
         else if(leftLCA!=NULL){
             return leftLCA;
         }
         else{
             return rightLCA;
         }
-        
     }
 };
