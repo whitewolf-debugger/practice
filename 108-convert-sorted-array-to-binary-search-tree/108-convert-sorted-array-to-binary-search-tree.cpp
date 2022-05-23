3c00eeda1135ca318ca9d0fb49034e41d242b634
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-05-23 17:01:50 +0530

    Time: 27 ms (26.21%), Space: 21.4 MB (42.52%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
@@ -12,12 +12,17 @@
 class Solution {
 public:
     TreeNode* createBST(vector<int> & nums , int start , int end){
+        //base case is that when start > end we  return NULL as the node
         if(start>end){
             return NULL;
         }
+        //The root is at the mid 
         int rootIndex = (start+end)/2;
+        // create a root with mid as the data 
         TreeNode* root = new TreeNode(nums[rootIndex]);
+        // now we want left child it will be from start to rootIndex-1
         root->left = createBST(nums,start,rootIndex-1);
+        //and right child are form rootIndex+1 to end 
         root->right = createBST(nums,rootIndex+1,end);
         return root;
     }
@@ -27,3 +32,18 @@ public:
         return createBST(nums,start,end);
     }
 };
+
+/*
+
+     1     	 2   	 3   	 4   	 5   	 6  	7
+     
+[left child from 1 to 3] [rootindex] [right child from 5 to 7]
+
+    1   2   3 
+    2 is the root and 1 is left child  3 is right child 
+    
+    5   6   7
+    6 is the root  and 5 is left child 7 is right child
+ 
+
+*/
