fff6f433feea82384e30126b7152a7ded2c0f1d1
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-05-26 00:38:42 +0530

    Time: 199 ms (6.71%), Space: 30.4 MB (9.84%) - LeetHub

--- 638-shopping-offers/638-shopping-offers.cpp
+++ 638-shopping-offers/638-shopping-offers.cpp
@@ -17,12 +17,15 @@ public:
     vector<int> updateNeeds(vector<int> special,vector<int> needs,char op){
         for(int i=0;i<needs.size();i++){
             if(op=='-'){
+                //if operation is - then we subtract the quantity of product needed with qty in the offer
                 needs[i]-=special[i];
             }
             else{
+                //if operation is + then we subtract the quantity of product needed with qty in the offer  for backtracking
                 needs[i]+=special[i];
             }
         }
+        //return the updated needs list
         return needs;
     }
     void dfs(vector<int>& price, vector<vector<int>>& special, vector<int>& needs,int currentPrice,int &ans){
@@ -32,17 +35,27 @@ public:
             ans = min(ans,currentPrice);
             return;
         }
+        //discovering every path from each index 
         for(int i=0;i<special.size();i++){
+            // if the quantity of product we need is eligible for the offer 
             if(isUsefulOffer(special[i],needs)){
+                //increase the current price 
                 currentPrice+=special[i].back();
+                //take that amount of products in offer 
                 needs = updateNeeds(special[i],needs,'-');
+                //discover price if we take this offer 
                 dfs(price,special,needs,currentPrice,ans);
+                //backtracking step revert back to original values of price and quantity needed
                 needs = updateNeeds(special[i],needs,'+');
+                //discover all other possibilities
                 currentPrice-=special[i].back();
             }
         }
+        //if at the end we check that the using the offer we didnot take all the needed items
         if(accumulate(needs.begin(), needs.end(), 0) != 0){
+            //take the needed items which are remaining and add their individual price to the current price 
             currentPrice+=remaining(needs,price);
+            //ans will be minimum of ans and current price 
             ans = min(ans,currentPrice);
             return;
         }
@@ -51,6 +64,7 @@ public:
         //ans is passed by reference stores the ans 
         int ans=INT_MAX;
         int currentPrice;
+        //do dfs and ans will be stored in ans
         dfs(price,special,needs,0,ans);
         return ans;
     }
