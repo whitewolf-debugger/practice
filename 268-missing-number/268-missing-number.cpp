4bcbff33f7dcf89de15644f0e2419819b1c08758
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-05-28 08:43:07 +0530

    Time: 53 ms (7.17%), Space: 24.1 MB (5.21%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -7,11 +7,13 @@ public:
         for(int i = 0 ; i < nums.size();i++){
             seen.insert(nums[i]);
         }
+        int ans;
         for(int i= low ;i <= high;i++){
             if(seen.find(i)==seen.end()){
-                return i;
+                ans = i;
+                break;
             } 
         }
-        return 0 ;
+        return ans ;
     }
 };
