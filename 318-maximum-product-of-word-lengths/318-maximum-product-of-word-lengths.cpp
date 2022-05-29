56e80953bba8f84168cdbf42219e787bb796b1f4
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-05-29 15:53:40 +0530

    Time: 134 ms (36.63%), Space: 15.8 MB (64.52%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     bool compare(bitset<26> &a, bitset<26> &b){
         for(int i=0;i<26;i++){
+            //check if there is 1 in any of bits from 1 to 26 im both  a and b
             if(a[i]==1 && b[i]==1) return true;
         }
+        //return false if there is no commmon letter 
         return false;
     }
     int maxProduct(vector<string>& words) {
@@ -12,11 +14,16 @@ public:
         vector<bitset<26>> chars(n);
         for(int i=0;i<words.size();i++){
             for(auto x: words[i]){
+                //visit every word and place 1 at that index where word is present in the vector
+                //{{1,1,0,0,0,0,0,0,..till 26 letters},..and so on till size n}
                 int index  = x -'a';
                 chars[i][index]=1;
             }
+            //check for more words and consecutively update the ans form 0 to i words which have been mapped
             for(int j = 0;j<i;j++){
+                //if there is no commong letter amont them 
                 if(!compare(chars[i],chars[j])){
+                    //ans  = max of ans and new calcualtion of size 
                     ans = max(ans, (int)words[i].size()*(int)words[j].size());
                 }
             }
