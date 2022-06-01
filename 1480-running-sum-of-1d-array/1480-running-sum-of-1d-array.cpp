c0428a14aafc900b7e5c7a87afb65af0673391de
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-01 09:23:02 +0530

    Time: 7 ms (28.96%), Space: 8.6 MB (38.06%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
+    void helper(vector<int>& nums,vector<int> &rs,int sum,int index){
+        if(index >= nums.size()){
+            return;
+        }
+        sum+=nums[index];
+        helper(nums,rs,sum,index+1);
+        rs[index]=sum;
+    }
     vector<int> runningSum(vector<int>& nums) {
         vector<int> rs(nums.size());
         int sum=0;
-        for(int i  = 0 ; i<nums.size();i++){
-            sum+=nums[i];
-            rs[i] = sum;
-        }
+        // for(int i  = 0 ; i<nums.size();i++){
+        //     sum+=nums[i];
+        //     rs[i] = sum;
+        // }
+        helper(nums,rs,sum,0);
         return rs;
     }
 };
