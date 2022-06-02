36135bc3982bb6856bee786dbf5d795c3d92cab4
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-02 18:55:47 +0530

    Time: 6 ms (98.49%), Space: 10.6 MB (78.16%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        //int n stores the number of rows of the matrix
         int n = matrix.size();
+        //m stores the number of columns of the matrix
         int m = matrix[0].size();
+        //sol stores the final transpose matrix
         vector<vector<int>> sol(m,vector<int>(n));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
+                // basic transpose logix aij=aji
                 sol[j][i] = matrix[i][j];
             }
         }
+        //return the ans
         return sol;
     }
 };
