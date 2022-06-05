e28a562e30ea57383203b945819f6fba306e380b
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-05 10:59:07 +0530

    Time: 1769 ms (14.29%), Space: 151.6 MB (14.29%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
@@ -4,17 +4,23 @@ public:
         unordered_map<int,int> m1;
         unordered_map<int,int> m2;
         for(int i=0;i<nums.size();i++){
+            //we are considering index from 1 since element with index = 0 means removed element  for us here 
             m1[nums[i]] = i+1;
             m2[i+1]= nums[i];
         }
         for(int i=0;i<operations.size();i++){
             vector<int> x = operations[i];
+            // changing element at that index
             m2[m1[x[0]]] = x[1];
+            //add new element to element : index map and index will be the index of previous element
             m1[x[1]] = m1[x[0]];
+            //set the index of previous element = 0 means not present 
             m1[x[0]]=0;
         }
+        //size of the ans will be same as the the size of intial array
         vector<int> ans(nums.size());
         for(int i =0;i<nums.size();i++){
+            //place the element at index i of ans 
             ans[i]=m2[i+1];
         }
         return ans;
@@ -27,6 +33,12 @@ public:
 1,3 , 2,1  3,2
 
 m1
+
+element    index
+1 	  ->  1
+2 	  ->  2
+
+
 element    index
 1 	  ->  2
 2 	  ->  1
@@ -35,6 +47,10 @@ element    index
 
 
 m2
+
+index  element
+1 	-> 	1
+2 	-> 	2
 
 index  element
 1 	-> 	2
