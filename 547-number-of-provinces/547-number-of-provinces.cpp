3f30ca0e710f82bb66731a70688e3e939cff59a9
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-07 13:52:41 +0530

    Time: 32 ms (55.54%), Space: 14.1 MB (32.26%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //graph bfs traversal
     void search(vector<vector<int>>& isConnected,int sv,vector<bool>& seen){
         queue<int> pendingNodes;
         pendingNodes.push(sv);
@@ -8,8 +9,11 @@ public:
             int front = pendingNodes.front();
             pendingNodes.pop();
             for(int i=0;i<isConnected.size();i++){
+                //if its not 1,1 position means pointing itself and it is unvisited also front has edge with i 
                 if(front != i && !seen[i] && isConnected[front][i]==1){
+                    //mark it as seen 
                     seen[i] = true;
+                    //push in pendingNodes and discover all paths 
                     pendingNodes.push(i);
                 }
             }
@@ -18,16 +22,20 @@ public:
     int BFS(vector<vector<int>>& isConnected,int n){
         vector<bool> seen(n,false);
         int count =0;
+        //cont number of disconnected nodes
         for(int i=0;i<n;i++){
             if(!seen[i]){
+                //discover all the nodes connected to current non traversed node
                 search(isConnected,i,seen);
                 count++;
             }
         }
+        //return total disconected node
         return count;
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
+        //ans is returned by BFS function which we return to main
         return BFS(isConnected,n);
     }
 };
