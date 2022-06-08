3f6f115e66904420cd1c4038cf75b3a529405bbb
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-08 09:45:09 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.79%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -4,10 +4,12 @@ public:
         int i =0;
         int j =s.size()-1;
         while(i<j){
+            //there are a and b only in the string we can remove them in 2 steps 
             if(s[i]!=s[j]) return 2;
             i++;
             j--;
         }
+        //if it comes out of the while loop means it is palindrome  and can be removed in 1 step 
         return 1;
     }
 };
