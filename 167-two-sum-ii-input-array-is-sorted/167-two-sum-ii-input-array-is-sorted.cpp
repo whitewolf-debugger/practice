3b21effd6f06fda9f258c015231825f587da044e
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-09 09:32:23 +0530

    Time: 41 ms (5.14%), Space: 15.7 MB (44.00%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        vector<vector<int>> solution;
          vector<int>index;
         unordered_map<int,int> mp;
+        // map all the elements with index+1
         for(int i=0;i<numbers.size();i++){
             mp[numbers[i]]=i+1;
         }
         int diff;
         for(int i=0;i<numbers.size();i++){
             diff = target - numbers[i];
+            //if index of difference != 0 means elementis there 
             if(mp[diff]!=0){
+                //push the i+1 index in index
                 index.push_back(i+1);
+                //push index of difference in the index 
                 index.push_back(mp[diff]);
+                //breakout
                 break;
             }
         }
+        //return ans
         return index;
     }
 };
