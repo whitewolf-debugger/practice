c4a104a2396df53f0478606f168bc486cfc31529
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-12 11:02:09 +0530

    Time: 858 ms (7.42%), Space: 127.1 MB (39.73%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        int curr_sum=0, res=0;
-		
-		//set to store the elements
-        unordered_set<int> st;
-        
-        int i=0,j=0;
-        while(j<nums.size()) {
-            while(st.count(nums[j])>0) {
-				//Removing the ith element untill we reach the repeating element
-                st.erase(nums[i]);
-                curr_sum-=nums[i];
+        unordered_set<int> seen;
+        int sum =0;
+        int ans = 0;
+        int start=0;
+        int i = 0;
+        while(i<nums.size()){
+            int curr  =  nums[i];
+            if(seen.find(curr)==seen.end()){
+                seen.insert(curr);
+                sum+=curr;
+                ans = max(ans,sum);
                 i++;
             }
-			//Add the current element to set and curr_sum value
-            curr_sum+=nums[j];
-            st.insert(nums[j]);
-            j++;
-			
-			//res variable to keep track of largest curr_sum encountered till now...
-            res = max(res, curr_sum);
+            else{
+                int del = nums[start];
+                seen.erase(del);
+                sum=sum - del;
+                start++;
+            }
         }
-        
-        return res;
+        return ans;
     }
 };
