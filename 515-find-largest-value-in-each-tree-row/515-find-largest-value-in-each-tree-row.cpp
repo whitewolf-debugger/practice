c1f69c3e0a9d587f50167364d94c4d7d59df5de1
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-13 01:05:52 +0530

    Time: 26 ms (18.43%), Space: 21.8 MB (99.95%) - LeetHub

--- 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.cpp
@@ -11,6 +11,7 @@
  */
 class Solution {
 public:
+    // finds out hte height of the tree 
     int height(TreeNode* root){
         if(root == NULL){
             return 0;
@@ -18,14 +19,19 @@ public:
         return 1+max(height(root->left),height(root->right));
     }
     void dfs(TreeNode *root,vector<int> &values,int level){
+        //base case 
         if(root ==NULL) return;
+        // max value at that index = max(values[level],root->val); where initally INT_MIN was placed
         values[level] = max(values[level],root->val);
+        // increase level as we go down
         dfs(root->left,values,level+1);
         dfs(root->right,values,level+1);
     }
     vector<int> largestValues(TreeNode* root) {
         int h = height(root);
+        // create vector of  size h put INT_MIN at every index
         vector<int> values(h,INT_MIN);
+        //do dfs 
         dfs(root,values,0);
         return values;
     }
