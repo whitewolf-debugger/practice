a6a79217495b524a50d21d0e56a2573bbcc8b114
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-12 11:04:29 +0530

    Time: 466 ms (56.38%), Space: 127.1 MB (35.12%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -8,19 +8,25 @@ public:
         int i = 0;
         while(i<nums.size()){
             int curr  =  nums[i];
+            // if current element is not in seen
             if(seen.find(curr)==seen.end()){
                 seen.insert(curr);
+                //add to current 
                 sum+=curr;
+                //update max
                 ans = max(ans,sum);
                 i++;
-            }
+            }//incase found
             else{
+                //remove the dublicate to make different  sequence 
                 int del = nums[start];
                 seen.erase(del);
                 sum=sum - del;
+                //increse start index once 1st element was found 
                 start++;
             }
         }
+        //return ans 
         return ans;
     }
 };
