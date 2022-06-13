c15f68aa7da8ca4eef186a5b17c5dba66e277c86
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-13 10:33:29 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (70.75%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int rows = triangle.size();
+        for(int r = 1 ; r < rows ;r++ ){
+            int n  = triangle[r].size();
+            for(int i = 0 ; i<n;i++){
+                if(i == 0){
+                    triangle[r][i]+= triangle[r-1][i];
+                }
+                else if(i==n-1){
+                    triangle[r][i]+=triangle[r-1][i-1];
+                }
+                else{
+                    triangle[r][i]+=min(triangle[r-1][i],triangle[r-1][i-1]);
+                }
+            }           
+        }  
+        int mn = INT_MAX;
+        for(int i = 0 ;i<triangle[rows-1].size();i++){
+            mn = min(triangle[rows-1][i],mn);
+        }
+        return mn;
+    }
+};
