43a2b201a8fd47668934f31e8c9e7d31ad7bfa4b
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-13 14:39:36 +0530

    Time: 343 ms (18.76%), Space: 51 MB (69.13%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -1,30 +1,42 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        vector<int> data;
-        ListNode* temp = head;
-        ListNode* temp2 = head;
-        while(temp!=NULL){
-            data.push_back(temp->val);
-            temp = temp->next;
+        if (!head || !head->next)
+            return head;
+        ListNode* mid = getMid(head);
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(mid);
+        return merge(left, right);
+    }
+
+    ListNode* merge(ListNode* list1, ListNode* list2) {
+        ListNode dummyHead(0);
+        ListNode* ptr = &dummyHead;
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                ptr->next = list1;
+                list1 = list1->next;
+            } else {
+                ptr->next = list2;
+                list2 = list2->next;
+            }
+            ptr = ptr->next;
         }
-        sort(data.begin(),data.end());
-        int i =0;
-        while(temp2!=NULL && i<data.size()){
-            temp2->val =data[i];
-            temp2 = temp2->next;
-            i++;
+        if(list1) ptr->next = list1;
+        else ptr->next = list2;
+
+        return dummyHead.next;
+    }
+
+    ListNode* getMid(ListNode* head) {
+        ListNode* midPrev = nullptr;
+        while (head && head->next) {
+            midPrev = (midPrev == nullptr) ? head : midPrev->next;
+            head = head->next->next;
         }
-        return head;
+        ListNode* mid = midPrev->next;
+        midPrev->next = nullptr;
+        return mid;
     }
 };
