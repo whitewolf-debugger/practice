d76f96dce41af751235f469b6ff40b671cf05a7d
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-17 20:17:02 +0530

    Time: 4 ms (98.44%), Space: 21.2 MB (55.06%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -17,21 +17,26 @@ public:
     2 = has camera
     */
     int dfs(TreeNode* root,int &cam){
+        // if the root is null means it is covered by camera so return 1 
         if(root == NULL) return 1;
         int left = dfs(root->left,cam);
         int right = dfs(root->right,cam);
+        // if any of the children is not covered add camera there and return 2 (means camera added)
         if(left == 0 || right ==0){
             cam++;
             return 2;
         }
+        // if any of the child has camera then return 1 (means this node is covered by that camera)
         else if( left == 2 || right == 2){
             return 1;
         }
+        // if that node not 1 or 0 than that node is not visited and not covered so return 0;
         else return 0;
     }
     int minCameraCover(TreeNode* root) {
         int cam = 0;
         int returned =  dfs(root,cam);
+        
         return returned == 0?cam+1:cam;
         
     }
