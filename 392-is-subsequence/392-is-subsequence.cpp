03f446ffb9409c7e6d9402ff9f715ddcca1ae531
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-19 13:21:46 +0530

    Time: 3 ms (55.90%), Space: 6.4 MB (35.06%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -2,14 +2,18 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         queue<char> q;
+        //push all the charracter of string s in a queue ;
         for(int i = 0;i<s.size();i++){
             q.push(s[i]);
         }
         for(int i = 0 ; i<t.size();i++){
+            //if the front of queue is = t[i] (since we want character to appear in order)
             if(q.front() == t[i]){
+                //remove that element from queue 
                 q.pop();
             }
         }
+        //if queue is empty then we got all the character of s in t in the correct order 
         return q.empty();
     }
 };
