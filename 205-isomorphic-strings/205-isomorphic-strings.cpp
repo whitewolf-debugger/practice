8368ea60f9a3a5d147568a459a8a51b43199e1f7
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-19 13:16:09 +0530

    Time: 18 ms (15.86%), Space: 7 MB (46.30%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -5,18 +5,22 @@ public:
         unordered_map<char,char> mp2;
         unordered_map<char,char> mp;
         for(int i=0;i<s.size();i++){
+            //if we encounter a character again while traversing then reutrn check for perfect mapping 
             if(mp[s[i]]!='\0'){
                if(mp[s[i]]!=t[i]) return false;
             }
             else{
+                //every character of string 1 maps to every character of string 2
                 mp[s[i]]=t[i];
             }
         }
         for(int i=0;i<s.size();i++){
+            //if there is repeat
             if(mp2[t[i]]!='\0'){
                if(mp2[t[i]]!=s[i]) return false;
             }
             else{
+                //every character of string 2 maps to every character of string 1
                 mp2[t[i]]=s[i];
             }
         }
