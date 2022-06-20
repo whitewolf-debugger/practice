fa098cfb7300c97fdf78d1e8d05c0a1c5fe6066e
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-20 11:02:12 +0530

    Time: 1042 ms (5.45%), Space: 285.9 MB (5.94%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -0,0 +1,33 @@
+
+class Solution {
+public:
+    static bool compare(string &s1,string &s2){
+        return s1.size() > s2.size();
+    }
+    int minimumLengthEncoding(vector<string>& words) {
+        sort(words.begin(),words.end(),compare);
+        if(words.size()==1){
+            return (words[0].size()+1);
+        }
+        unordered_map<string,string> mp;
+        string ans = "";
+        for(int i = 0; i<words.size();i++){
+            string word = words[i];
+            if(mp[word]!="\0"){
+                continue;
+            }
+            else{
+                ans=ans+word+"#";
+                for(int j = 0 ;j<word.size();j++){
+                    string key = word.substr(j);
+                    mp[key] = word;  
+                    cout<<key<<":"<<word<<endl;
+                }
+            }
+        }
+       // int last = ans.size()-1;
+        //if(ans[last]!='#') ans = ans+"#";
+        cout<<ans;
+        return ans.size();
+    }
+};
