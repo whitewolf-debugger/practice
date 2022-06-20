d0d74b6baef99cd0d66c6d93fb9a0aaa28e93e3a
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-20 11:11:16 +0530

    Time: 700 ms (5.45%), Space: 285.7 MB (5.94%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -7,28 +7,35 @@ public:
     int minimumLengthEncoding(vector<string>& words) {
         //sorting words accordiong to their size 
         sort(words.begin(),words.end(),compare);
-        //base case if there is only one word in the strinf
-        // if(words.size()==1){
-        //     return (words[0].size()+1);
-        // }
+        //map of substring of words vs the original word 
         unordered_map<string,string> mp;
+        //stores the final ans 
         string ans = "";
+        //iterating through all the words
         for(int i = 0; i<words.size();i++){
+            //stores the word at that index
             string word = words[i];
+            //if that word is not in the map means it is suffix  example "me" is suffix of time 
             if(mp[word]!="\0"){
+                //then we skip 
                 continue;
             }
+            //if it is not prefix
             else{
+                //add that word and # after that in ans 
                 ans=ans+word+"#";
+                //map all the substring of word and the orginal word (helps to check suffix)
                 for(int j = 0 ;j<word.size();j++){
                     string key = word.substr(j);
                     mp[key] = word;
-                    //creating a map of key like time ime me e  
-                    cout<<key<<":"<<word<<endl;
+                    //creating a map of key like time ime me e with original word 
+                    //cout<<key<<":"<<word<<endl;
                 }
             }
         }
-        cout<<ans;
+        
+        //cout<<ans;
+        //return size of the ans
         return ans.size();
     }
 };
