7ef2a0b2df32384dd22eef9018559c27598f0f7c
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-20 11:07:40 +0530

    Time: 704 ms (5.45%), Space: 285.8 MB (5.94%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+    //this fuction helps us to sort the words according to their size 
     static bool compare(string &s1,string &s2){
         return s1.size() > s2.size();
     }
     int minimumLengthEncoding(vector<string>& words) {
+        //sorting words accordiong to their size 
         sort(words.begin(),words.end(),compare);
-        if(words.size()==1){
-            return (words[0].size()+1);
-        }
+        //base case if there is only one word in the strinf
+        // if(words.size()==1){
+        //     return (words[0].size()+1);
+        // }
         unordered_map<string,string> mp;
         string ans = "";
         for(int i = 0; i<words.size();i++){
@@ -19,7 +22,8 @@ public:
                 ans=ans+word+"#";
                 for(int j = 0 ;j<word.size();j++){
                     string key = word.substr(j);
-                    mp[key] = word;  
+                    mp[key] = word;
+                    //creating a map of key like time ime me e  
                     cout<<key<<":"<<word<<endl;
                 }
             }
