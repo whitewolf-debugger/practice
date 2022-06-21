f4690307fb199ec3f7782387aacfb60827b752ac
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-21 16:47:57 +0530

    Time: 11 ms (17.89%), Space: 11 MB (74.82%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //doing binary search which is O(log N)
     int binarySearch(vector<int>& nums,int target,int start,int end){
         int mid;
         while(start<=end){
@@ -13,14 +14,19 @@ public:
     int search(vector<int>& nums, int target) {
         int L =0;
         int R =1;
+        //find the index where the is previous number  greater than the next one since that is point of division of 2 sorted arrays 
         while(R<nums.size() && nums[L]<nums[R]){
             L++;
             R++;
         }
+        //call binary search on left which is 0 to L
         int Left = binarySearch(nums,target,0,L);
+        //call binary search on right which is R to nums.size()-1 (end)
         int Right = binarySearch(nums,target,R,nums.size()-1);
+        //if either of left or right is not -1 means we found our target return that target
         if(Left!=-1) return Left;
         if(Right!=-1) return Right;
+        //if we didnot find our target we return -1
         return -1;
     }
 };
