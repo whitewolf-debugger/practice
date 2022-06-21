2a9ac1baa969ac4186987fba6e32a5656e5fd15c
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-21 18:10:23 +0530

    Time: 4 ms (43.45%), Space: 8.3 MB (70.34%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        //creating a map for counting total number of 0 1 and 2 s 
         unordered_map<int,int> mp;
         int start =0;
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
         }
+        //start helps to keep a check  of index and placing that much number of  0s 1s and 2s 
         for(int i =0;i<mp[0];i++){
             nums[start++]=0;
         }
