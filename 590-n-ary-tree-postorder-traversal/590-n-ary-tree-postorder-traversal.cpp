0175713abac341b128c3a4538b2ee6bdc13d337c
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-23 23:04:25 +0530

    Time: 22 ms (92.00%), Space: 11.2 MB (81.08%) - LeetHub

--- 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.cpp
+++ 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.cpp
@@ -20,11 +20,13 @@ public:
 
 class Solution {
 public:
+    //postorder traversal using dfs
     void dfs(Node* root,vector<int>& ans){
         if(root==NULL) return;
         for(int i=0;i<root->children.size();i++){
             dfs(root->children[i],ans);
         }
+        //pushing root after traversal 
         ans.push_back(root->val);
     }
     vector<int> postorder(Node* root) {
