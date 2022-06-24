31f88e77dd0d401cfefb85e1f7a034405ac13f14
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-24 12:41:37 +0530

    Time: 72 ms (41.26%), Space: 27.4 MB (97.90%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -4,15 +4,21 @@ public:
         long int maxIndex = 0;
         long int sum =0;
         for(int i =0;i<target.size();i++){
+            //find out sum of all element
             sum+=target[i];
             if(target[i]>target[maxIndex]){
+                //find out index of largest element
                 maxIndex = i;
             }
         }
+        //find difference of maxvalue with the total sum 
         long int diff = sum - target[maxIndex];
+        // base cases 
         if(target[maxIndex]==1 || diff ==1) return true;
         if(diff>target[maxIndex] || diff ==0 || target[maxIndex]%diff==0) return false;
+        //replacing the element at maxindex with 1 
         target[maxIndex]%=diff;
+        //recirsive call 
         return isPossible(target);
     }
 };
