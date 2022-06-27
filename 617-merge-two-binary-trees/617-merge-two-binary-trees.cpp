1a25e42bb289155074e8089366b6f09878fd249b
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-28 00:18:51 +0530

    Time: 88 ms (7.71%), Space: 32.3 MB (87.49%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -11,12 +11,17 @@
  */
 class Solution {
 public:
+    //we make change in 1st tree and return its head 
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        //if one of the node is null return the other node
         if(root1==nullptr) return root2;
         if(root2==nullptr) return root1;
+        //else  value of node 1 is added with value at that node 2
         root1->val = root1->val+root2->val;
+        //attatch left and right to the root1 
         root1->left = mergeTrees(root1->left,root2->left);
         root1->right = mergeTrees(root1->right,root2->right);
+        //return ans
         return root1;
     }
 };
