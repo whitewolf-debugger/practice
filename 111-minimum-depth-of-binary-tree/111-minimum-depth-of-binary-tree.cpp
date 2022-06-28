c79d3ec923478d8126b69993fbf6c3de70386188
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-28 19:51:25 +0530

    Time: 478 ms (14.81%), Space: 144.7 MB (43.63%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -12,18 +12,24 @@
 class Solution {
 public:
     void dfs(TreeNode* root,int &ans,int level){
+        //base case of dfs 
         if(!root) return;
+        //if left and right of root is NULL means we are at the leaf updae the ans 
         if(root->right==NULL && root->left==NULL){
             ans = min(ans,level+1);
         }
+        //dfs calls on left and right and increase the level 
         dfs(root->left,ans,level+1);
         dfs(root->right,ans,level+1);
     }
     int minDepth(TreeNode* root) {
         if(!root) return 0;
+        //put ans as INT_MAX since we want minimum depth;
         int ans =INT_MAX;
         int level =0;
+        //do dfs 
         dfs(root,ans,level);
+        //return ans 
         return ans;
     }
 };
