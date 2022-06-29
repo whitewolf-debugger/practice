33ce100c0e81538d2064824ffb93fb5911136659
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-29 11:01:18 +0530

    Time: 9 ms (59.53%), Space: 13.7 MB (51.13%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -4,6 +4,7 @@ public:
         int n=cost.size();
         cost.push_back(0);
         for(int i=2;i<=n;i++){
+            //create an array which is sum of current element + minimum of last 2 elements and return n 
             cost[i]=min(cost[i]+cost[i-1],cost[i]+cost[i-2]);
         }
         return cost[n];
