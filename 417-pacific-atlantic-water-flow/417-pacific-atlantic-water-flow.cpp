c7635c73119c2e4555e59749d820bc66eb72c8b2
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-29 17:44:08 +0530

    Time: 81 ms (37.22%), Space: 19.4 MB (42.29%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     void dfs(vector<vector<int>> &heights,vector<vector<int>>& ocean,int prev,int i,int j,int m,int n){
+        //base case if  we are exceeding the boundary or when  ocean[i][j]==1
         if(i<0 || j<0 || i>=m || j>=n || ocean[i][j]==1) return;
+        //base case if prev>heights[i][j] we do not do anything 
         if(prev>heights[i][j]) return;
+        //place 1 at that place in the index 
         ocean[i][j]=1;
+        //calls on all 4 directions up down left right 
         dfs(heights,ocean,heights[i][j],i+1,j,m,n);
         dfs(heights,ocean,heights[i][j],i-1,j,m,n);
         dfs(heights,ocean,heights[i][j],i,j+1,m,n);
@@ -28,6 +32,7 @@ public:
             //last column call for atlantic 
             dfs(heights,toAtlantic,INT_MIN,i,n-1,m,n);
         }
+        //pushing intersections of both in the ans vector 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(toPacific[i][j]==1 && toAtlantic[i][j]==1){
