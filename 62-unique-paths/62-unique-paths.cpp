37328b676a00984fd1f99415a8ed0bca8cb0339e
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-29 11:52:31 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.32%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -10,3 +10,14 @@ public:
         return paths[m-1][n-1];
     }
 };
+/*
+    place 1 in every  index since we want to fill 1st row and 1st column with 1 
+    now 
+    1 1 1 1
+    1 2 3 4
+    1 3 6 10
+    1 4 10 20
+    to reach end 20 ways are there 
+    since 
+    number of ways to reach [i][j] =  number of ways to reach[i-1][j]+ number of ways to reach[i][j-1];
+*/
