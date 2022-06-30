a6d035a135b1c19b064900217a8a0b3a6b8acdb9
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-06-30 09:54:17 +0530

    Time: 13 ms (75.41%), Space: 10.7 MB (92.09%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
+        //sort the array 
         sort(nums.begin(),nums.end());
         int start =0;
         int end = nums.size();
+        //get mid index 
         int mid = (start+end)/2;
+        //get middle element 
         int element = nums[mid];
         int ans  =0;
+        //element less than mid are smaller since it is sorted arry so add the difference element-nums[i] (number of steps)
         for(int i=0;i<mid;i++){
             ans+=(element-nums[i]);
         }
+         //element greater than mid are smaller since it is sorted arry so add the difference nums[j]-element (number of steps)
         for(int j=mid+1;j<end;j++){
             ans+=(nums[j]-element);
         }
+        //return ans
         return ans;
     }
 };
 /*
 
-
- 1   2   9  10
- 1   0   7   8 
- 1   2   3
-
+ elements of the array -:
+ 1   2   9  10     
+ 
+     mid = 4/2  =2
+     
+     mid element  =9;
+     
+ absolute difference of each element from mid -:
+ 8   7   0   1
 */
