f5b11b3fefa76a1cd3e5b1f025b0a608ca3ebbb4
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-07-02 14:52:18 +0530

    Time: 78 ms (93.66%), Space: 32.3 MB (40.04%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
+        //ans is the product between maximunm maximum height difference and maxmimum width distance
         horizontalCuts.push_back(0);
         horizontalCuts.push_back(h);
         verticalCuts.push_back(0);
         verticalCuts.push_back(w);
+        //we are pushing in the boundaries since they also act as a cut lines 
         sort(horizontalCuts.begin(),horizontalCuts.end());
         sort(verticalCuts.begin(),verticalCuts.end());
+        //sort all the cut line 
         int size1 = horizontalCuts.size();
         int size2 = verticalCuts.size();
         int a = 0;
         int b = 0;
+        //find out maximum diff b/w 2 consecutive horizontal lines 
         for(int i=0;i<size1-1;i++){
-            a = max(a,horizontalCuts[i+1]-horizontalCuts[i])%1000000007;
+            a = max(a,horizontalCuts[i+1]-horizontalCuts[i]);
         }
+        //find out maximum diff b/w 2 consecutive horizontal lines 
         for(int j=0;j<size2-1;j++){
-            b = max(b,verticalCuts[j+1]-verticalCuts[j])%1000000007;
+            b = max(b,verticalCuts[j+1]-verticalCuts[j]);
         }
-        
+        //convert product to long long and reuturn after doing modulo 
         return (1LL*a*b)%1000000007;
     }
 };
