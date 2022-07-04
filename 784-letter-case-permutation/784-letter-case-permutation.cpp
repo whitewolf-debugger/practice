eb83bfbc4511bc741135bb4cee32d210018aab70
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-07-04 10:55:02 +0530

    Time: 11 ms (54.99%), Space: 11.5 MB (44.02%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.cpp
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    void dfs(string &s,vector<string>&path,int i){
-        //base case when current index is more than the size of the element
-        if(i >= s.size()){
-            //push the  string in path
-            path.push_back(s);
+    void dfs(string s, vector<string> &ans,int start){
+        if(start>=s.size()){
+            ans.push_back(s);
             return;
-        }
-        // if element at current index in the string is 
-        if(isalpha(s[i])){
-            //first convert to lower case explore all the path
-            s[i]=tolower(s[i]);
-            dfs(s,path,i+1);
-            //convert to upper case explore all the path
-            s[i]=toupper(s[i]);
-            dfs(s,path,i+1);
+        } 
+        if(isalpha(s[start])){
+            s[start]=toupper(s[start]);
+            dfs(s,ans,start+1);
+            s[start]=tolower(s[start]);
+            dfs(s,ans,start+1);  
         }
         else{
-            //if element at that index is digit  we do nothing move to element at next index by i+1
-            dfs(s,path,i+1);
+            dfs(s,ans,start+1);
         }
     }
     vector<string> letterCasePermutation(string s) {
-        vector<string> path;
-        //ans is stored in path since we are passing path by reference
-        dfs(s,path,0);
-        //return path with ans
-        return path;
+        vector<string> ans;
+        dfs(s,ans,0);
+        return ans;
     }
 };
