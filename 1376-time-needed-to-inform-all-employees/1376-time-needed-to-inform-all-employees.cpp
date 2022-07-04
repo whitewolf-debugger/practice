c1ad39ce27baf16dcc42ffadc6be5e00e6253af9
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-07-04 13:41:02 +0530

    Time: 292 ms (90.76%), Space: 122.1 MB (64.65%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
+    //we do dfs to find height of Nary tree
     int dfs(int headID,vector<vector<int>> &adj,vector<int> &informtime){
+        //do it like maximum height of Nary tree 
         int time =0;
+        //travel through all the nodes which are working under head get the max time out 
         for(auto x:adj[headID]){
             time = max(time,dfs(x,adj,informtime));
         }
+        //return time + time taken to inform by the head
         return time+informtime[headID];
     }
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         vector<vector<int>> adj(n);
+        //make adjacency list pushing all the worker under manager in vector present at manager[i] of vector<vector>>
         for(int i=0;i<n;i++){
             if(manager[i]!=-1){
                 adj[manager[i]].push_back(i);
