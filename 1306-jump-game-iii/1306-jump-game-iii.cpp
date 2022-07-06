47b67b56293e42df48849286d465e42fd4ba564d
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-07-06 09:14:34 +0530

    Time: 177 ms (5.01%), Space: 48.5 MB (11.12%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -4,10 +4,13 @@ public:
         int n = arr.size();
         vector<vector<int>> adj(n);
         vector<bool> visited(n,false);
+        //make sdjacency list 
         for(int i=0;i<arr.size();i++){
             adj[i].push_back(arr[i]);
         }
+        //make queue for bfs 
         queue<int> pendingNodes;
+        //push the start in pendingNodes
         pendingNodes.push(start);
         while(!pendingNodes.empty()){
             int front = pendingNodes.front();
@@ -15,11 +18,13 @@ public:
             if(arr[front]==0){
                 return true;
             }
+            //get the new next index push it in pendingNodes if next is within the range 
             int next = front+arr[front];
             if(next>=0 && next<n && !visited[next]){
                 visited[next]=true;
                 pendingNodes.push(next);
             }
+            //get the new next index push it in pendingNodes if next is within the range 
             next = front-arr[front];
             if(next>=0 && next<n && !visited[next]){
                 visited[next]=true;
