a3b78dc932242acd4cde1e2767f9468c6aa834bd
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-07-07 15:40:51 +0530

    Time: 6 ms (9.32%), Space: 6.7 MB (41.92%) - LeetHub

--- 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
@@ -1,3 +1,17 @@
+/*
+
+Steps -
+
+1. Make a map to mark all the mutations present in the bank
+2. Make another map to mark all the visited mutations
+3. Make a queue of strings and push the starting mutation in queue
+4. Do a BFS traversal, until the queue is not empty
+7. Pop the currunt mutation out of the string
+8.If the current mutation is end mutation, return ans
+9.For each character in the mutation, change each gene to get a new mutation and insert each new mutation in queue only if the new mutation is not visited and is present in the bank
+10.I the queue becomes empty and we do not get the end mutation, ans is -1
+
+*/
 class Solution {
 public:
     int minMutation(string start, string end, vector<string>& bank) {
