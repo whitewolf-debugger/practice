e05f82c6462c8a4370ccb646369df7caa82d9a4c
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-07-11 22:57:57 +0530

    Time: 17 ms (5.32%), Space: 11.9 MB (66.66%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -12,18 +12,17 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans;
-        if(!root) return ans;
         queue<TreeNode*> pendingNodes;
         pendingNodes.push(root);
+        vector<int> ans;
+        if(!root) return ans;
         while(!pendingNodes.empty()){
-            int size = pendingNodes.size();
-            //bfs 
+            int size=pendingNodes.size();
+            vector<int> temp;
             while(size--){
                 TreeNode* front = pendingNodes.front();
                 pendingNodes.pop();
-                //push right most node in ans 
-                if(size==0) ans.push_back(front->val);
+                temp.push_back(front->val);
                 if(front->left!=NULL){
                     pendingNodes.push(front->left);
                 }
@@ -31,8 +30,8 @@ public:
                     pendingNodes.push(front->right);
                 }
             }
+            ans.push_back(temp[temp.size()-1]);
         }
-        //return ans
         return ans;
     }
 };
