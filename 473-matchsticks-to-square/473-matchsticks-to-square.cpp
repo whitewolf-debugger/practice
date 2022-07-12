1d4451f15cf636f9b2d0746fcb8044e3519a4890
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-07-12 10:04:11 +0530

    Time: 522 ms (42.70%), Space: 10.2 MB (30.06%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    int side1,side2,side3,side4;
-    bool makesquare(vector<int>& matchsticks,int currIndex){
+    bool makesquare(vector<int>& matchsticks,int currIndex,int side1,int side2,int side3,int side4){
         if(currIndex==matchsticks.size()){
             if(side1==0 && side2==0 && side3==0 && side4 ==0){
                 return true;
@@ -10,22 +9,22 @@ public:
         }
         if(matchsticks[currIndex]<=side1){
             side1-=matchsticks[currIndex];
-            if(makesquare(matchsticks,currIndex+1)) return true;
+            if(makesquare(matchsticks,currIndex+1,side1,side2,side3,side4)) return true;
             side1+=matchsticks[currIndex];
         }
         if(matchsticks[currIndex]<=side2){
              side2-=matchsticks[currIndex];
-            if(makesquare(matchsticks,currIndex+1)) return true;
+            if(makesquare(matchsticks,currIndex+1,side1,side2,side3,side4)) return true;
             side2+=matchsticks[currIndex];
         }
         if(matchsticks[currIndex]<=side3){
             side3-=matchsticks[currIndex];
-            if(makesquare(matchsticks,currIndex+1)) return true;
+            if(makesquare(matchsticks,currIndex+1,side1,side2,side3,side4)) return true;
             side3+=matchsticks[currIndex];
         }
         if(matchsticks[currIndex]<=side4){
             side4-=matchsticks[currIndex];
-            if(makesquare(matchsticks,currIndex+1)) return true;
+            if(makesquare(matchsticks,currIndex+1,side1,side2,side3,side4)) return true;
             side4+=matchsticks[currIndex];
         }
         return false;
@@ -35,12 +34,8 @@ public:
         if(matchsticks.size()<4) return false;
         int totalsum = accumulate(matchsticks.begin(),matchsticks.end(),0);
         if(totalsum%4!=0) return false;
-        int sidesum = totalsum/4;
+        int target = totalsum/4;
         sort(matchsticks.rbegin(),matchsticks.rend());
-        side1 = sidesum;
-        side2 = sidesum;
-       side3 = sidesum;
-        side4 = sidesum;
-        return makesquare(matchsticks,0);
+        return makesquare(matchsticks,0,target,target,target,target);
     }
 };
