1a618dfcd9967fbd995c9a847c93c7eaefb0126e
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-07-13 23:30:38 +0530

    Time: 59 ms (99.02%), Space: 23.9 MB (29.16%) - LeetHub

--- 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
@@ -9,11 +9,17 @@ public:
             p.push_back({pairs[i][0],pairs[i][1]});
         }
         int ans =0;
+        //sort the vector p in order of end limit 
         sort(p.begin(),p.end(),compare);
+        //assign last as the end limit but first assign it as the minimum element 
         int last = INT_MIN;
+        //iterate through every pair
         for(auto & pii: p){
+            // last is upper limit of previous pair if it is lower than the start of current pair means it can be in chain
             if(last<pii.first){
+                //assign last as the end limit of the current element
                 last = pii.second;
+                //increase the size of chain 
                 ans++;
             }
         }
