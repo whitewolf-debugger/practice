d1e3308486069d9cac01a3b12596976afb1db953
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-07-16 00:36:35 +0530

    Time: 7 ms (11.09%), Space: 7.7 MB (10.95%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.cpp
+++ 140-word-break-ii/140-word-break-ii.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     void backtrack(string s,string temp,unordered_set<string> dict,vector<string> &ans){
+        //if we have iterated through the entire size of the string
         if(s=="") {
+            //remove gap from the end 
             temp.pop_back();
+            //push the temp string in the ans vector 
             ans.push_back(temp);
             return;
         }
         string prefix;
         for(int i=0;i<s.size();i++){
+            //get the prefix 
             prefix = prefix+s[i];
+            //if it is present in the list 
             if(dict.find(prefix)!=dict.end()){
+                //add prefix to the temp 
                 backtrack(s.substr(i+1),temp+prefix+" ",dict,ans);
             }
         }
