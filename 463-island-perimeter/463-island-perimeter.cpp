a4e92178d8d2db249eb269dc1f933926a81f2515
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-07-16 08:18:20 +0530

    Time: 185 ms (44.89%), Space: 96.4 MB (40.81%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.cpp
+++ 463-island-perimeter/463-island-perimeter.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     int dfs(vector<vector<int>>& grid,int i,int j,int ROWS,int COLS){
-        // if we are at the perimeter return 1 
         if(i<0 || j<0 || i>=ROWS || j>=COLS || grid[i][j]==0) return 1;
-        //if visited it is -1 then return 0
         if(grid[i][j]==-1) return 0;
-        //mark visited
         grid[i][j]=-1;
-        //return ans after doing 4 direction dfs calls 
         return dfs(grid,i+1,j,ROWS,COLS)+dfs(grid,i-1,j,ROWS,COLS)+dfs(grid,i,j+1,ROWS,COLS)+dfs(grid,i,j-1,ROWS,COLS) ;
     }
     int islandPerimeter(vector<vector<int>>& grid) {
@@ -16,14 +12,11 @@ public:
         int ans =0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                //if that node has 1 it is unvisited 
                 if(grid[i][j]==1){
-                    //do dfs 
                     ans = dfs(grid,i,j,m,n);
                 }
             }
         }
-        //ans stores ans 
         return ans;
     }
 };
