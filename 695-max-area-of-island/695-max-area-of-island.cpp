e8f4752ede7a8223c2de4049ec865e08780666f0
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-07-16 08:18:41 +0530

    Time: 20 ms (86.81%), Space: 23.9 MB (29.56%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,56 +1,31 @@
 class Solution {
 public:
-    //question same as number of islands 
-    int maxAreaOfIsland(vector<vector<int>>& grid){
+    void dfs(vector<vector<int>> &grid,vector<vector<bool>> & visited,int i,int j,int ROWS,int COLS,int &area){
+        if(i<0 || j<0 || i>=ROWS || j>=COLS || visited[i][j] || grid[i][j]!=1){
+            return;
+        }
+        visited[i][j]=true;
+        area++;
+        dfs(grid,visited,i+1,j,ROWS,COLS,area);
+        dfs(grid,visited,i,j+1,ROWS,COLS,area);
+        dfs(grid,visited,i,j-1,ROWS,COLS,area);
+        dfs(grid,visited,i-1,j,ROWS,COLS,area);
+        
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        //helps to keep track of visited array
-        vector<vector<bool>> seen(m,vector<bool>(n,false));
-        //helps to traverse all the directions 
-        vector<pair<int,int>> dir = {{0,1},{1,0},{-1,0},{0,-1}};
-        // it is final ans
-        int ans =0;
-        //visiting every block
+        int ans = 0;
+        vector<vector<bool>> visited(m,vector<bool>(n,false));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                //checking whether 1 is there in in that i j and check whether we have seen that 
-                if(grid[i][j]==1 && !seen[i][j]){
-                    //count = 0
-                    int count = 0;
-                    //doing dfs using queue 
-                    queue<pair<int,int>> pendingNodes;
-                    //pushing that row col in queue
-                    int row = i;
-                    int col = j;
-                    pendingNodes.push({row,col});
-                    //mark that index as seen 
-                    seen[i][j] = true;
-                    while(!pendingNodes.empty()){
-                        pair<int,int> p = pendingNodes.front();
-                        int r = p.first;
-                        int c = p.second;
-                        pendingNodes.pop();
-                        //increase count as we visit ever node connected to current node and it has 1 
-                        count++;
-                        //checking all the direction arround that block {{0,1},{1,0},{-1,0},{0,-1}}
-                        for(pair<int,int> &d:dir){
-                            int row = r + d.first;
-                            int col = c + d.second;
-                            // the direction which we are checking for is in limit  and 1 is placed there and we have not visited that 
-                            if(row>=0 && col>=0 && row<m && col<n && !seen[row][col] && grid[row][col]==1){
-                                // push that row col in queue where we found 1 (for dfs)
-                                pendingNodes.push({row,col});
-                                //mark that index as seen 
-                                seen[row][col]=true;
-                            }
-                        }
-                    }
-                    //update the max size 
-                    ans = max(ans,count);
+                if(!visited[i][j] && grid[i][j]==1){
+                    int area =0;
+                    dfs(grid,visited,i,j,m,n,area);
+                    ans = max(ans,area);
                 }
             }
         }
-        //return the final ans 
         return ans;
     }
 };
