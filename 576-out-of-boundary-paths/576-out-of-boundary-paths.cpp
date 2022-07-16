a634d41580250d90fc3c179b2dc144adfad70404
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-07-16 16:45:17 +0530

    Time: 163 ms (5.42%), Space: 104.6 MB (5.03%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -7,20 +7,20 @@ public:
         }
         return false;
     }
-    int dfs(int i,int j,int m, int n, int moves,vector<vector<vector<int>>> &dp){
+    int dfs(int i,int j,int m, int n, int moves,vector<vector<vector<int>>> &memo){
         if(!isInside(i,j,m,n)){
             return 1;
         }
         if(moves<=0) return 0;
-         if(dp[i][j][moves]!=-1) return dp[i][j][moves];
-        int left =dfs(i-1,j,m,n,moves-1,dp);
-        int right =dfs(i+1,j,m,n,moves-1,dp);
-        int up =dfs(i,j-1,m,n,moves-1,dp);
-        int down =dfs(i,j+1,m,n,moves-1,dp);
-        return dp[i][j][moves] = (left%mod+right%mod+up%mod+down%mod)%mod;
+         if(memo[i][j][moves]!=-1) return memo[i][j][moves];
+        int left =dfs(i-1,j,m,n,moves-1,memo);
+        int right =dfs(i+1,j,m,n,moves-1,memo);
+        int up =dfs(i,j-1,m,n,moves-1,memo);
+        int down =dfs(i,j+1,m,n,moves-1,memo);
+        return memo[i][j][moves] = (left%mod+right%mod+up%mod+down%mod)%mod;
     }
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        vector<vector<vector<int>>> dp(51,vector<vector<int>>(51,vector<int>(51,-1)));
-        return dfs(startRow,startColumn,m,n,maxMove,dp);
+        vector<vector<vector<int>>> memo(51,vector<vector<int>>(51,vector<int>(51,-1)));
+        return dfs(startRow,startColumn,m,n,maxMove,memo);
     }
 };
