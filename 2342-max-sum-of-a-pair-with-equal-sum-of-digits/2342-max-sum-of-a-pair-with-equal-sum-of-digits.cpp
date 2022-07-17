cb68f9c18e387232a23b0c5e7a43946987aaf8b7
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-07-17 09:39:24 +0530

    Time: 357 ms (100.00%), Space: 73.3 MB (100.00%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -10,18 +10,24 @@ public:
         return ans;
     }
     int maximumSum(vector<int>& nums) {
-        vector<pair<int,int>> pairs;
         int ans =-1;
+        //make map of all the sum of digits  and numbers  
         unordered_map<int,vector<int>> mp;
         vector<int> digsum(nums.size(),0);
         for(int i=0;i<nums.size();i++){
+            //calculate the digit sum 
             digsum[i]=Digsum(nums[i]);
+            //push the number at that index 
             mp[digsum[i]].push_back(nums[i]);
         }
         for(auto& x:mp){
+            //elements sotres the elements that have same digit sum and different index 
             vector<int> elements  = x.second;
+            //sort it in descending order 
             sort(elements.rbegin(),elements.rend());
+            //if there are no 2 elements with same digit sum so we donot do anything for that digitsum 
             if(elements.size()<2) continue;
+            //1st 2 element are greateast so add them and compare with ans 
             int sum  = elements[0]+elements[1];
             ans = max(ans,sum);
         }
