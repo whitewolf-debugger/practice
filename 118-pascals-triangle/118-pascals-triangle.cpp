bfb7e8cb6da9923cd105df1625b3a7e43cbc803a
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-07-19 20:24:51 +0530

    Time: 4 ms (25.27%), Space: 6.5 MB (71.06%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -16,16 +16,20 @@ public:
         ans.push_back({1});
         ans.push_back({1,1});
         int level =3;
+        //we will be traversing in ans 2D vector at the row less than the current row (wich we are making)
         for(int i=1;i<n-1;i++){
             vector<int> v;
             for(int j=0;j<level;j++){
                 if(j==0){
+                    //push 1 at the 1st index of the vector 
                     v.push_back(1);
                 }
                 else if(j == level-1){
+                        //push 1 at the last index of the vector
                         v.push_back(1);
                 }
                 else{
+                    //else push ans[i][j]+ans[i][j-1]
                     v.push_back(ans[i][j]+ans[i][j-1]);
                 }
             }
