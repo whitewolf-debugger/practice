a7faee3fefcf6b44ce31d83d6c501667106bf745
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-07-19 20:17:52 +0530

    Time: 8 ms (5.00%), Space: 6.7 MB (31.97%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        int n = numRows;
+        vector<vector<int>> ans;
+        if (n==0) return ans;
+        if(n==1){
+            ans.push_back({1});
+            return ans;
+        }
+        if(n==2){
+            ans.push_back({1});
+            ans.push_back({1,1});
+            return ans;
+        }
+        ans.push_back({1});
+        ans.push_back({1,1});
+        int level =2;
+        for(int i=1;i<n-1;i++){
+            vector<int> v;
+            for(int j=0;j<=level;j++){
+                if(j==0){
+                    v.push_back(ans[i][0]);
+                }
+                else if(j == level){
+                        v.push_back(ans[i][level-1]);
+                }
+                else{
+                    v.push_back(ans[i][j]+ans[i][j-1]);
+                }
+            }
+            ++level;
+            ans.push_back(v);
+            
+        }
+        return ans;
+    }
+};
