e7577c958cea491535113a602e540e1d36c63eed
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-07-31 23:32:36 +0530

    Time: 638 ms (62.03%), Space: 153.1 MB (24.33%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -1,86 +1,61 @@
-struct segmenttree {
-	int n;
-	vector<int> st;
-
-	void init(int _n) {
-		this->n = _n;
-		st.resize(4 * n, 0);
-	}
-
-	void build(int start, int ending, int node, vector<int> &v) {
-		// leaf node base case
-		if (start == ending) {
-			st[node] = v[start];
-			return;
-		}
-
-		int mid = (start + ending) / 2;
-
-		// left subtree is (start,mid)
-		build(start, mid, 2 * node + 1, v);
-
-		// right subtree is (mid+1,ending)
-		build(mid + 1, ending, 2 * node + 2, v);
-
-		st[node] = st[node * 2 + 1] + st[node * 2 + 2];
-	}
-
-	int query(int start, int ending, int l, int r, int node) {
-		// non overlapping case
-		if (start > r || ending < l) {
-			return 0;
-		}
-
-		// complete overlap
-		if (start >= l && ending <= r) {
-			return st[node];
-		}
-
-		// partial case
-		int mid = (start + ending) / 2;
-
-		int q1 = query(start, mid, l, r, 2 * node + 1);
-		int q2 = query(mid + 1, ending, l, r, 2 * node + 2);
-
-		return q1 + q2;
-	}
-
-	void update(int start, int ending, int node, int index, int value) {
-		// base case
-		if (start == ending) {
-			st[node] = value;
-			return;
-		}
-
-		int mid = (start + ending) / 2;
-		if (index <= mid) {
-			// left subtree
-			update(start, mid, 2 * node + 1, index, value);
-		}
-		else {
-			// right
-			update(mid + 1, ending, 2 * node + 2, index, value);
-		}
-
-		st[node] = st[node * 2 + 1] + st[node * 2 + 2];
-
-		return;
-	}
-
-	void build(vector<int> &v) {
-		build(0, n - 1, 0, v);
-	}
-
-	int query(int l, int r) {
-		return query(0, n - 1, l, r, 0);
-	}
-
-	void update(int x, int y) {
-		update(0, n - 1, 0, x, y);
-	}
-    
+struct segmenttree{
+    vector<int> st;
+    int n;
+    void init(int n){
+        this -> n = n;
+        st.resize(4*n,0);
+    }
+    void build(vector<int>& v,int start,int end,int node){
+        if(start == end){
+            st[node]=v[start];
+            return;
+        }
+        int mid = (start + end) /2;
+        //building left subtree 
+        build(v,start,mid,2*node+1);
+        //building right subtree
+        build(v,mid+1,end,2*node+2);
+        //going up and adding up child nodes and placing it in the empty position
+        st[node]=st[2*node+1]+st[2*node+2];
+    }
+    int query(int l,int r,int start,int end,int node){
+        //no overlapping
+        if(start>r || end<l) return 0;
+        //complete overlapping
+        if(start>=l && end<=r){
+            return st[node];
+        }
+        //partial overlapping 
+        int mid = (start+end)/2;
+        int q1 = query(l,r,start,mid,2*node+1);
+        int q2 = query(l,r,mid+1,end,2*node+2);
+        return q1+q2;
+    }
+    void update(int index,int val,int start,int end,int node){
+        if(start ==end){
+            st[node] = val;
+            return;
+        }
+        int mid = (start+end)/2;
+        if(mid>=index){
+            update(index,val,start,mid,2*node+1);
+        }
+        else{
+            update(index,val,mid+1,end,2*node+2);
+        }
+        st[node]=st[2*node+1]+st[2*node+2];
+        return;
+    }
+    void build(vector<int> v){
+        build(v,0,n-1,0);
+    }
+    int query(int l,int r){
+        return query(l,r,0,n-1,0);
+    }
+    void update(int index,int val){
+        update(index,val,0,n-1,0);
+    }
 };
-
 class NumArray {
 public:
     segmenttree tree;
