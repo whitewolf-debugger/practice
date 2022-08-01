9b033d2983428cdaf1d5a15ef27a9694a904b630
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-08-01 11:03:29 +0530

    Time: 3 ms (44.81%), Space: 6.6 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> paths(m,vector<int>(n,1));
+        vector<vector<int>> grid(m,vector<int>(n,1));
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                paths[i][j] = paths[i-1][j]+paths[i][j-1];
+                grid[i][j]=grid[i-1][j]+grid[i][j-1];
             }
         }
-        return paths[m-1][n-1];
+        return grid[m-1][n-1];
     }
 };
-/*
-    place 1 in every  index since we want to fill 1st row and 1st column with 1 
-    now 
-    1 1 1 1
-    1 2 3 4
-    1 3 6 10
-    1 4 10 20
-    to reach end 20 ways are there 
-    since 
-    number of ways to reach [i][j] =  number of ways to reach[i-1][j]+ number of ways to reach[i][j-1];
-*/
