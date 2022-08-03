496513eb8eeeeafadbed62be1384b854e023ef3b
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-08-03 21:25:20 +0530

    Time: 105 ms (89.54%), Space: 47.5 MB (6.38%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,3 +1,7 @@
+//creating  node of the tree where each node has 2 values 
+/*left children have StartDte and EndDate lower than the startDate and endDate of the parent 
+right childrens have StartDte and EndDate greater than the startDate and endDate of the parent 
+*/
 class Node{
     public:
     int startDate;
@@ -14,29 +18,40 @@ class Node{
 class MyCalendar {
     public:
     Node* root;
+    //checks wheteher we can book or not 
     bool book(Node* root ,int start,int end){
+        //if there is an empty space to book then we return true
         if(!root) return true;
+        // if the start date of current root(movie) is after the end (end || startDate || endDate)
         else if(root->startDate>=end){
+            //check of left is null means there is an empty place  return true
             if(!root->left){
                 return true;
             }
+            // left is not empty so we search for empty place in lefy since start date of current root is after the end  
             else{
                 return book(root->left,start,end);
             }
         }
+        // if the start date of current root(movie) is after the end (startDate || endDate || starts)
         else if(root->endDate<=start){
+            //then we check if there is place in right if there is place then return true;
             if(!root->right){
                 return true;
             }
-            else{
+            //look for free time in right
+            else{         
                 return book(root->right,start,end);
             }
         }
+        //if we donot get any free space then return false
         else{
             return false;
         }
     }
+    //update helps to update the nodes 
     Node* update(Node* node,int start,int end){
+        //if there is n
         if(!node){
             Node* newNode = new Node(start,end);
             return newNode;
