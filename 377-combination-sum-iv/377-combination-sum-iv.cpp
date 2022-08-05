28b99688b0a8ba8831f9386ad63c301b4f68dc2e
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-08-05 06:52:32 +0530

    Time: 9 ms (10.81%), Space: 6.8 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
+    //like knapsack style solution 
     int dfs(vector<int>& nums,int k,vector<int> & cache){
+        //if k==0 means we have our target
         if(k==0){
+            //return 1
             return 1;
         }
+        //return cache of its already there (memoization)
         if(cache[k]!=-1)return cache[k];
         int howmanyWays =0;
+        //x is nums 
         for(auto x: nums){
+            //if(k>=x) we do dfs get the combination
             if(k>=x){
                 howmanyWays+=dfs(nums,k-x,cache);
             }
         }
+        //cache the ways we got 
         return cache[k] = howmanyWays;
     }
     int combinationSum4(vector<int>& nums, int target) {
+        //memoization of the solution 
         vector<int>  cache(target+1,-1);
         vector<int> v;
         int ans = dfs(nums,target,cache);
