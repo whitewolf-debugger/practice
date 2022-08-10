8ec38ab38b0828077a1d7dae6373d5ef7bb1ee5a
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-08-10 06:45:55 +0530

    Time: 143 ms (5.05%), Space: 342.7 MB (8.06%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
@@ -10,40 +10,18 @@
  * };
  */
 class Solution {
-public:
-    TreeNode* createBST(vector<int> & nums , int start , int end){
-        //base case is that when start > end we  return NULL as the node
-        if(start>end){
-            return NULL;
-        }
-        //The root is at the mid 
-        int rootIndex = (start+end)/2;
-        // create a root with mid as the data 
-        TreeNode* root = new TreeNode(nums[rootIndex]);
-        // now we want left child it will be from start to rootIndex-1
-        root->left = createBST(nums,start,rootIndex-1);
-        //and right child are form rootIndex+1 to end 
-        root->right = createBST(nums,rootIndex+1,end);
+    TreeNode* work(vector<int> nums,int L,int R){
+        if(R<L) return NULL;
+        int M =(R+L)/2;
+        TreeNode * root = new TreeNode(nums[M]);
+        
+        root->left = work(nums,L,M-1);
+        root->right = work(nums,M+1,R);
         return root;
     }
+public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        int end = nums.size()-1;
-        int start =0;
-        return createBST(nums,start,end);
+        int n = nums.size();
+        return work(nums, 0, n-1);
     }
 };
-
-/*
-
-     1     	 2   	 3   	 4   	 5   	 6  	7
-     
-[left child from 1 to 3] [rootindex] [right child from 5 to 7]
-
-    1   2   3 
-    2 is the root and 1 is left child  3 is right child 
-    
-    5   6   7
-    6 is the root  and 5 is left child 7 is right child
- 
-
-*/
