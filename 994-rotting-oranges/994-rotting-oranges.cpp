a25dafedcee325debb13939f38314dad1da810d3
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-08-12 20:45:27 +0530

    Time: 23 ms (10.74%), Space: 13.4 MB (24.47%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,41 +1,44 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int ROWS= grid.size();
+        
+        int ROWS = grid.size();
         int COLS = grid[0].size();
-        int total =0;
-        vector<pair<int,int>> directions = {{0,-1},{-1,0},{1,0},{0,1}};
-        queue<pair<int,int>> pendingNodes;
+        int count = 0;
+        int total = 0;
+        int minutes = 0;
+        queue<pair<int , int>> pendingNodes;
+        vector<pair<int , int>> directions = {{0 , 1},{1 , 0} , {-1 , 0} ,{0 , -1}};
         vector<vector<bool>> visited(ROWS,vector<bool>(COLS,false));
-        for(int i=0;i<ROWS;i++){
-            for(int j=0;j<COLS;j++){
-                if(grid[i][j]!=0) total++;
-                if(grid[i][j]==2){
-                    pendingNodes.push({i,j});
+        
+        for(int i = 0; i < ROWS; i++){
+            for(int j =0; j < COLS; j++){
+                if(grid[i][j] != 0) total++;
+                if(grid[i][j] == 2){
+                    pendingNodes.push({i , j});
                 }
             }
         }
-        int minutes =0;
-        int count =0;
+        
         while(!pendingNodes.empty()){
-            int size = pendingNodes.size();
+            int size  = pendingNodes.size();
             count+=size;
-            for(int i=0;i<size;i++){
-                pair<int,int> front = pendingNodes.front();
-                int row= front.first;
-                int col=front.second;
+            for(int i = 0; i < size; i++) {
+                pair<int , int> top = pendingNodes.front();
+                int row = top.first;
+                int col = top.second;
                 pendingNodes.pop();
-                for(auto& dir: directions){
-                    int newrow = row + dir.first;
-                    int newcol = col + dir.second;
-                    if(newrow>=0 && newcol>=0 && newrow<ROWS && newcol < COLS && grid[newrow][newcol]==1){
-                        grid[newrow][newcol]=2;
-                        pendingNodes.push({newrow,newcol});
+                for(auto &dir : directions) {
+                    int i = row + dir.first;
+                    int j = col + dir.second;
+                    if(i<ROWS && j<COLS && i>=0 && j>=0 && !visited[i][j] && grid[i][j]==1) {
+                        pendingNodes.push({i,j});
+                        visited[i][j] = true;
                     }
                 }
-            }
+            }   
             if(!pendingNodes.empty()) minutes++;
         }
-        return (count == total)? minutes:-1;
+        return (total == count)?minutes:-1;
     }
 };
