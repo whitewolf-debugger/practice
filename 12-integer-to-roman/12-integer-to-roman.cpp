9ca9af6446c5f7e5e8371e7ec8112088afbff519
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-08-16 07:25:02 +0530

    Time: 3 ms (96.50%), Space: 9.5 MB (15.88%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+           vector<pair<int, string> > nums;
+        
+        nums = {{1000, "M"},
+            {900, "CM"},
+            {500, "D"}, 
+            {400, "CD"}, 
+            {100, "C"},
+            {90, "XC"}, 
+            {50, "L"}, 
+            {40, "XL"}, 
+            {10, "X"}, 
+            {9, "IX"}, 
+            {5, "V"},
+            {4, "IV"}, 
+            {1, "I"}
+        };
+
+    string ans = "";
+    while (num != 0) {
+        for (auto &x : nums) {
+            if (num >= x.first) {
+                ans = ans + x.second;
+                num = num - x.first;
+                break;
+            }
+        }
+    }
+
+    return ans;
+    }
+};
