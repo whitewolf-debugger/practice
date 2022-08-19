2727f8145547e3661d7110a5eda31e52db65e88f
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-08-19 23:41:47 +0530

    Time: 192 ms (36.60%), Space: 58.2 MB (66.47%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -2,58 +2,51 @@ class Solution {
 public:
     bool isPossible(vector<int>& nums) {
         
+        //stires the frequency of each element
         unordered_map<int ,int> freqMap;
+        //stores the frequency of next needed element
         unordered_map<int ,int> need;
         int n = nums.size();
         
+        //making map of elements
         for(int i =0; i < n; i++){
             freqMap[nums[i]]++;
         }
         
+        //traversing in the nums array
         for(int &num: nums){
+            //if elements frequency is 0 then we do not do naything
             if(freqMap[num] == 0) continue;
             
+            //if there is need of current element full fill it and do need[num]-- and increase need for next element need[num+1]++
             else if(need[num] > 0){
                 need[num]--;
                 freqMap[num]--;
-                 need[num+1]++;
+                need[num+1]++;
                 
-            } else if(freqMap[num] >0 && freqMap[num+1] >0 && freqMap[num+2] >0 ){
-                
+            }else if(freqMap[num] >0 && freqMap[num+1] >0 && freqMap[num+2] >0 ){
+                //take next 3 element hypthetically and increase need for next element i.e num+3
                 freqMap[num]--;
                 freqMap[num+1]--;
                 freqMap[num+2]--;
-                 need[num+3]++;
+                need[num+3]++;
                 
             } else{
+                //if all these are not possible return false;
                 return false;
             }
         }
         return true;
     }
 };
-/*
 
-    1,  2,  3,  3,  4,  4,  5,  5
-            ^                            
-    unordered_map<int , int> mp = {
-        1:0
-        2:0
-        3:1
-        4:1
-        5:1
-    }
-    
-    
-    unordered_map<int,int> um ={
-    
-        3:1
-    }
+/* 
+    for each num in nums first try placing it in one of the existing subseq.
+    if no subseq. needs that number.
+    then, try creating a new subseq. of atleast length 3 starting with that num.
 
-1. map the frequency
-2. check whether frequency of current element is greater than 0
-3. if frequency of current element is greater than 0 then check for frequncies of next consecutive elements
-
+    if neither of the two condtn is true, we return false
+    since, that num can't be a part of any subseq.
 
 
 */
