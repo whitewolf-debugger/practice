f5b9337e7b0116d00c482a7026bad754d3a02a33
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-08-22 22:09:11 +0530

    Time: 3 ms (58.38%), Space: 7.8 MB (63.86%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dfs(vector<int>& nums,int ind, int lastind,vector<int> &dp){
+        if(ind > lastind){
+            return 0;
+        }
+        if(dp[ind] != -1) return dp[ind];
+        //not take
+        int nottake = dfs(nums,ind+1,lastind,dp);
+        int take = nums[ind];
+        //take
+        if(ind+2 <= lastind) {
+            take += dfs(nums,ind+2,lastind,dp);
+        }
+        return dp[ind] = max(take,nottake);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        vector<int> dp1(n+1 , -1);
+        vector<int> dp2(n+1 , -1);
+        return max(dfs(nums,1,n-1,dp1) , dfs(nums,0,n-2,dp2));
+    }
+};
