5966604bdbb2693e482cd9a18d53b5e3fa671231
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-08-25 00:48:19 +0530

    Time: 791 ms (15.38%), Space: 17.9 MB (13.24%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     long long dp(vector<int>& nums,int index,vector<int>& cache){
+        
+        //if we are at the end return return 0
         if(index >= nums.size()-1){
             return 0;
         }
         
+        //memoization step using cache 
         if(cache[index] != -1) return cache[index];
+        
+        //ans stores the minimum step
         long long ans = INT_MAX;
+        
         for(int i =1; i <= nums[index]; i++){
+            
+            //move stores the moves it takes if we take 'i' step 
             long long move = 1+dp(nums,index+i,cache);
+            //we want minimum of those steps
             ans = min(ans,move);
         }
+        
+        //memoization 
         return cache[index] = ans;
     }
     int jump(vector<int>& nums) {
