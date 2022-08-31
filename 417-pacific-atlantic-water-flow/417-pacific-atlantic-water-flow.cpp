6197154372c89f718f4d9f360f6bac15735c3752
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-08-31 19:39:41 +0530

    Time: 49 ms (87.15%), Space: 19.3 MB (43.37%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,48 +1,58 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &heights,vector<vector<int>>& ocean,int prev,int i,int j,int m,int n){
-        //base case if  we are exceeding the boundary or when  ocean[i][j]==1
-        if(i<0 || j<0 || i>=m || j>=n || ocean[i][j]==1) return;
-        //base case if prev>heights[i][j] we do not do anything 
-        if(prev>heights[i][j]) return;
-        //place 1 at that place in the index 
-        ocean[i][j]=1;
-        //calls on all 4 directions up down left right 
-        dfs(heights,ocean,heights[i][j],i+1,j,m,n);
-        dfs(heights,ocean,heights[i][j],i-1,j,m,n);
-        dfs(heights,ocean,heights[i][j],i,j+1,m,n);
-        dfs(heights,ocean,heights[i][j],i,j-1,m,n);
+    void dfs(vector<vector<int>> &heights,int i,int j, int ROWS, int COLS,vector<vector<int>> &ocean,int prev){
+        if(i < 0 || j < 0 || i >= ROWS || j >= COLS || ocean[i][j] == 1){
+            return;
+        }
+        
+        if(heights[i][j] < prev) return;
+        
+        ocean[i][j] = 1;
+        
+        dfs(heights,i + 1,j,ROWS,COLS,ocean,heights[i][j]);
+        dfs(heights,i,j + 1,ROWS,COLS,ocean,heights[i][j]);
+        dfs(heights,i - 1,j,ROWS,COLS,ocean,heights[i][j]);
+        dfs(heights,i,j - 1,ROWS,COLS,ocean,heights[i][j]);
+        
     }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        
+        int ROWS = heights.size();
+        int COLS = heights[0].size();
+        
+        vector<vector<int>> toPacific(ROWS,vector<int>(COLS,0));
+        vector<vector<int>> toAtlantic(ROWS,vector<int>(COLS,0));
         vector<vector<int>> ans;
-        if(heights.size()<0) return ans;
-        int m = heights.size();
-        int n = heights[0].size();
-        vector<vector<int>> toPacific(m,vector<int>(n,0));
-        vector<vector<int>> toAtlantic(m,vector<int>(n,0));
-        for(int i=0;i<n;i++){
-            //first row call for pacific
-            dfs(heights,toPacific,INT_MIN,0,i,m,n);
-            //last row call for atlantic 
-            dfs(heights,toAtlantic,INT_MIN,m-1,i,m,n);
+        
+        for(int i = 0; i < ROWS; i++){
+            //first column for the pacific ocean
+            dfs(heights,i,0,ROWS,COLS,toPacific,-1);
+                
+            //last column for the atlantic ocean
+            dfs(heights,i,COLS-1,ROWS,COLS,toAtlantic,-1);
         }
-        for(int i=0;i<m;i++){
-             //first column call for pacific
-            dfs(heights,toPacific,INT_MIN,i,0,m,n);
-            //last column call for atlantic 
-            dfs(heights,toAtlantic,INT_MIN,i,n-1,m,n);
+        
+        for(int i = 0; i < COLS; i++){
+            //first row for the pacific ocean
+            dfs(heights,0,i,ROWS,COLS,toPacific,-1);
+                
+            //last column for the atlantic ocean
+            dfs(heights,ROWS-1,i,ROWS,COLS,toAtlantic,-1);
         }
-        //pushing intersections of both in the ans vector 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(toPacific[i][j]==1 && toAtlantic[i][j]==1){
-                    vector<int> v(2);
-                    v[0]=i;
-                    v[1]=j;
+        
+        for(int i = 0; i < ROWS; i++){
+            for(int j = 0; j < COLS; j++){
+                if(toPacific[i][j] == 1 && toAtlantic[i][j] == 1){
+                    vector<int> v(2,0);
+                    v[0] = i;
+                    v[1] = j;
                     ans.push_back(v);
+                    
                 }
             }
         }
+        
         return ans;
+        
     }
 };
