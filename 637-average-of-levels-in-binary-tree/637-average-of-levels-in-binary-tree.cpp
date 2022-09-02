7593512ed7c389ec735fd8470ad712f226446ff3
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-09-02 21:49:49 +0530

    Time: 20 ms (73.50%), Space: 22.5 MB (32.47%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
@@ -16,27 +16,42 @@ public:
         pendingNodes.push(root);
         while(!pendingNodes.empty()){
             int size = pendingNodes.size();
+            //sum stores the sum for every level 
             double sum  = 0;
+            //count counts the sum for every level 
             double count = 0;
+            //we use size to travel the entire level and size is updated every time 
             while(size--){
+                //get the front of the queue 
                 TreeNode* front = pendingNodes.front();
                 pendingNodes.pop();
+                //count the number of elements 
                 count++;
+                //count the sum of the level
                 sum += front -> val;
+    
+                //push the left in the queue if it is there 
                 if(front -> left != NULL){
                     pendingNodes.push(front -> left);
                 }
+                //push the right in the queue if it exists 
                 if(front -> right != NULL){
                     pendingNodes.push(front -> right);
                 }
             }
+            //calculate the average of the level
             double avg  = sum/count;
+            //push the average in the level 
             ans.push_back(avg);
         }
     }
     vector<double> averageOfLevels(TreeNode* root) {
+        //ans stores the sum of the level 
         vector<double> ans;
+        //do the level order traversal 
         LevelOrder(root,ans);
+        
+        //return the ans vector 
         return ans;
     }
 };
