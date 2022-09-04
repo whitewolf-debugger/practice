cd6831655e193b1f7e6179e6be70be3bed363f76
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-09-04 13:45:10 +0530

    Time: 14 ms (20.06%), Space: 13.6 MB (49.15%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -15,23 +15,36 @@ public:
         
         map<int,map<int,multiset<int>>> nodeMap;
         queue<pair<TreeNode*,pair<int,int>>> pendingNodes;
+        // initially the coordinates are {0,0}
         pendingNodes.push({root,{0,0}});
         
+        //while the queue is not empty 
         while(!pendingNodes.empty()){
+            
+            //take the current front node 
             TreeNode* currNode = pendingNodes.front().first;
+            
+            //store the current x and y coordinates 
             int x = pendingNodes.front().second.first;
             int y = pendingNodes.front().second.second;
+            
+            //remove the front element of the queue 
             pendingNodes.pop();
+            
+            //add the current node rto x and y in the map 
             nodeMap[x][y].insert(currNode->val);
             
+            // for left x-1 and y+1 as we go down along with going to the left 
             if(currNode -> left != NULL){
                 pendingNodes.push({currNode -> left,{x-1,y+1}});
             }
+            // for right x+1 and y+1 as we go down along with going to the right
             if(currNode -> right != NULL){
                 pendingNodes.push({currNode -> right,{x+1,y+1}});
             }
         }
         
+        //create the ans vector by pushing all the element of given set of x and y in ans array
         vector<vector<int>> ans;
         for(auto x: nodeMap){
             vector<int> col;
@@ -40,9 +53,11 @@ public:
                     col.push_back(element);
                 }
             }
+            
+            //push the currant column in the ans 
             ans.push_back(col);
         }
-        
+        //return the ans
         return ans;
     }
 };
