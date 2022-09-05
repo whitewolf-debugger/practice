de7394382874d12ddeea5e84eacce16e53aeeb10
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-09-05 08:45:44 +0530

    Time: 841 ms (50.00%), Space: 412.3 MB (50.00%) - LeetHub

--- 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     long long MOD = 1e9 + 7;
     long long dfs(int startPos, int endPos, int k,vector<vector<long long>> &dp){
+        
+        //if we get number of size k and starPost = endPosition return 1 else return 0;
         if(k == 0){
             if(startPos == endPos){
                 return 1;
@@ -9,13 +11,15 @@ public:
                 return 0;
             }
         }
+        //memoization step
         if(dp[startPos+1000][k] != -1) return  dp[startPos+1000][k];
         long long ways  = 0;
-        // reduce the start
+        //reduce the start
         ways = (ways % MOD + dfs(startPos-1,endPos,k-1,dp) % MOD) % MOD;
         //increase the start
         ways = (ways % MOD + dfs(startPos+1,endPos,k-1,dp) % MOD) % MOD;
         
+        //return the numebr of ways by memoizing it 
         return dp[startPos+1000][k] = ways;
     }
     int numberOfWays(int startPos, int endPos, int k) {
