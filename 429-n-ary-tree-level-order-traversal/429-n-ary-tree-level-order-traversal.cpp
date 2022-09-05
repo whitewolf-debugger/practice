71f267acd9247f80efcd4c5ad2802da2140991ea
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-09-05 07:37:45 +0530

    Time: 58 ms (5.44%), Space: 11.8 MB (65.30%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -21,24 +21,36 @@ public:
 class Solution {
 public:
     vector<vector<int>> levelOrder(Node* root) {
+        
+        //check ifthe root is NULL then return an empty vector
         if(!root) return {};
         vector<vector<int>> ans;
         queue<Node*> pendingNodes;
         vector<int> level;
+        //push the root in pendingNodes queue
         pendingNodes.push(root);
+        //while pendingNode has data in it 
         while(!pendingNodes.empty()){
+            //get the size of the queue pendingNodes
             int size = pendingNodes.size();
             level.clear();
             while(size--){
+                //get the front node from pendingNodes
                 Node* front = pendingNodes.front();
+                //push the value in level vector 
                 level.push_back(front -> val);
+                //pop the front node
                 pendingNodes.pop();
+                
+                //peush all the children of current node from left ti right in the pendingNodes
                 for(int i = 0; i < front -> children.size();i++){
                     pendingNodes.push(front->children[i]);
                 }
             }
+            //push the level in the ans
             ans.push_back(level);
         }
+        //return ans
         return ans;
     }
 };
