18ad936b20d1aeacac181e9cb7f3478d20b7365a
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-09-08 19:29:57 +0530

    Time: 14 ms (32.19%), Space: 8.9 MB (76.19%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.cpp
+++ 174-dungeon-game/174-dungeon-game.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     int dfs(vector<vector<int>> &grid,int i, int j,int ROWS, int COLS,vector<vector<int>> &dp){
+        //if any how we cross the boundary return INT_MAX
         if(i < 0 || j < 0 || i >= ROWS || j >= COLS){
             return INT_MAX;
         }
+        //if we are at the end . the end position might have orbs or demon 
         if(i == ROWS-1 && j == COLS-1){
             return grid[i][j] <= 0? abs(grid[i][j])+1 : 1;
         }
+        //memoization step
         if(dp[i][j] != -1) return dp[i][j];
+        
+        //option 1 go right 
         int right = dfs(grid,i+1,j,ROWS,COLS,dp);
+        
+        //option 2 go down
         int down = dfs(grid,i,j+1,ROWS,COLS,dp);
-
+        
+        //get the result which is minimum of the right and down and then subrtact it with grid[i][j]
         int result = min(right, down) - grid[i][j];
+        
+        //memoization , it the result is negative means player is dead then return 1 else return the result
         return dp[i][j] = (result <= 0) ? 1 : result;   
     }
     int calculateMinimumHP(vector<vector<int>>& grid) {
