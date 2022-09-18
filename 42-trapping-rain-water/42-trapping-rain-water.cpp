97fe09a1c52e6153751e8bbc874d3bbd17c56206
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-09-18 07:52:47 +0530

    Time: 21 ms (72.47%), Space: 20.4 MB (40.49%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -17,3 +17,29 @@ public:
         return ans;
     }
 };
+/*
+   0   1   0   2   1   0   1   3   2   1   2   1
+
+   |         |    
+   |         |  index =  2
+   |         |
+   |         |
+   |____1____|
+   |____0____|
+
+ now  after poping the top element
+
+  0   1   0   2   1   0   1   3   2   1   2   1
+
+   |         |    
+   |         |  index =  2
+   |         |  top = 1
+   |         |  st.top() = 0            2           1                   0
+   |         |  height_bound = min(height[index],height[st.top()]) - height[top]; = 1
+   |____0____|  distance = index - st.top() - 1; = 1
+                area = distance * height_bound;
+
+
+
+
+*/
