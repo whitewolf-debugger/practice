7a9cb2ec9811a5ea17c97e8e281860bcdaacc7e7
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-09-28 17:57:09 +0530

    Time: 250 ms (37.05%), Space: 41 MB (51.96%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -5,6 +5,7 @@ public:
         priority_queue<pair<int,int>,vector<pair<int,int>> , greater<pair<int,int>>> pq;
         //push the source along with its weight in the priority queue 
         pq.push({0,source});
+        //we keep the distance of source from source to be 0
         signalReceivedAt[source] = 0;
         //same as BFS approach 
         while(!pq.empty()) {
@@ -12,12 +13,14 @@ public:
             int currNodeTime = pq.top().first;
             int currentNode = pq.top().second;
             pq.pop();
-            
-            //if(currNodeTime > signalReceivedAt[currentNode]) continue;
+            //check for the neighbouring edges 
             for(auto & edge : adj[currentNode]) {
+                //get the weight of path from current node to  neighbour 
                 int time = edge.first;
                 int nbr = edge.second;
+                //check if that time to reach from source  we had earlier is more than the time we get by adding (current time + this time)
                 if(signalReceivedAt[nbr] > time + currNodeTime) {
+                    //update the time to reach that node 
                     signalReceivedAt[nbr] = time + currNodeTime;
                     pq.push({signalReceivedAt[nbr],nbr});
                 }
@@ -30,9 +33,10 @@ public:
             int source = time[0];
             int destination = time[1];
             int weight = time[2];
+            //make adjacency list having source :{weight,destination}
             adj[source].push_back({weight,destination});
         }
-        
+        //stores the time(weight) from source to the nodes
         vector<int> signalReceivedAt(n+1,INT_MAX);
         int ans = INT_MIN;
         dijkstras(adj,signalReceivedAt,k,n);
