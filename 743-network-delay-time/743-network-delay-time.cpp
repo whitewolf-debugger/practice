36aee74b39db12f58bd69f1e6e6e06f6a0a2fe16
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-09-28 17:51:22 +0530

    Time: 234 ms (45.00%), Space: 41 MB (51.96%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,57 +1,40 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>>graph(n+1);
-      priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        for(int i=0;i<times.size();i++){
-            graph[times[i][0]].push_back({times[i][1],times[i][2]});
-            
-        }
-        vector<int>vis(n+1,0);
-        vector<int>parent(n+1,-1);
-        vector<int>dist(n+1,INT_MAX);
-        parent[k]=k;
-        dist[k]=0;
-        vis[k]=1;
-        for(int i=0;i<graph[k].size();i++){
-            pq.push({graph[k][i].second,graph[k][i].first});
-            if(dist[k]+graph[k][i].second<dist[graph[k][i].first]){
-                  dist[graph[k][i].first]=  dist[k]+graph[k][i].second;
-                    parent[graph[k][i].first]=k;
+    void dijkstras(vector<vector<pair<int,int>>> adj,vector<int> &signalReceivedAt,int source,int n) {
+        priority_queue<pair<int,int>,vector<pair<int,int>> , greater<pair<int,int>>> pq;
+        pq.push({0,source});
+        signalReceivedAt[source] = 0;
+        while(!pq.empty()) {
+            int currNodeTime = pq.top().first;
+            int currentNode = pq.top().second;
+            pq.pop();
+            if(currNodeTime > signalReceivedAt[currentNode]) continue;
+            for(auto & edge : adj[currentNode]) {
+                int time = edge.first;
+                int nbr = edge.second;
+                if(signalReceivedAt[nbr] > time + currNodeTime) {
+                    signalReceivedAt[nbr] = time + currNodeTime;
+                    pq.push({signalReceivedAt[nbr],nbr});
                 }
+            }
+        }
+    }
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int,int>>> adj(n+1);
+        for(auto &time : times) {
+            int source = time[0];
+            int destination = time[1];
+            int weight = time[2];
+            adj[source].push_back({weight,destination});
         }
         
-        while(pq.empty()!=true){
-           pair<int, int> cur=pq.top();
-    pq.pop();
-            // cout<<cur.second<<cur.first<<endl;
-           if(vis[cur.second ]==0){
-           
-            for(int i=0;i<graph[cur.second].size();i++){
-                // cout<<graph[cur.second][i].first<<endl;
-                  if(vis[graph[cur.second][i].first]==0){
-                if(dist[cur.second]+graph[cur.second][i].second<dist[graph[cur.second][i].first]){
-                  dist[graph[cur.second][i].first]=  dist[cur.second]+graph[cur.second][i].second;
- pq.push({  dist[graph[cur.second][i].first],graph[cur.second][i].first});
-                    parent[graph[cur.second][i].first]=cur.second;
-                      }
-                }
-            }
-            }
-            vis[cur.second]=1;
+        vector<int> signalReceivedAt(n+1,INT_MAX);
+        int ans = INT_MIN;
+        dijkstras(adj,signalReceivedAt,k,n);
+        for(int i =1; i <= n ;i++) {
+            ans = max(ans,signalReceivedAt[i]);
         }
-        int m=INT_MIN;
-        for(int i=1;i<dist.size();i++){
-            // cout<<dist[i]<<" ";
-            if(m<dist[i]){
-                m=dist[i];
-            }
-        }
-        if(m!=INT_MAX){
-            return m;
-        }
-        else{
-            return -1;
-        }
+        return ans == INT_MAX ? -1 : ans;
+        
     }
 };
