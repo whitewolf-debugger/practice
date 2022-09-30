6529e23bc2431ffb4d9803d63d336729704c9881
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-09-30 20:26:14 +0530

    Time: 342 ms (34.87%), Space: 28.6 MB (21.74%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //do DFS 
     void search(vector<vector<int>> & adj,vector<bool> &visited,int sv) {
         visited[sv] = true;
         for(auto & neighbor: adj[sv]) {
@@ -11,6 +12,7 @@ public:
     }
     int dfs(vector<vector<int>> & adj, int n) {
         int ans = 0;
+        //we do dfs for every bomb to get maximum chain explosion 
         for(int i = 0 ; i < n;i++) {
             vector<bool> visited(n,false);
             int count = 0;
@@ -32,6 +34,7 @@ public:
             vector<int> b1  = bombs[i];
             for(int j = i + 1 ; j < n;j++) {
                 vector<int> b2  = bombs[j];
+                //if another bomb gets exploded due to the current bomb so current bomb becomes parent 
                 long long distance = pow((b2[0] - b1[0]),2) + pow((b2[1] - b1[1]),2);
                 if(distance <= pow(b1[2],2)) {
                     adj[i].push_back(j);
