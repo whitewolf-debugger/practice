b3c6e81f34a232148d575f7de87d2876adf3df0e
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-10-07 00:02:02 +0530

    Time: 824 ms (12.13%), Space: 128.6 MB (50.64%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -1,28 +1,29 @@
 class TimeMap {
 public:
-    unordered_map<string,unordered_map<int,string>> mp;
+    unordered_map<string, unordered_map<int, string>> keyTimeMap;
     TimeMap() {
-        
     }
     
     void set(string key, string value, int timestamp) {
-        mp[key][timestamp] = value;
+        // Store '(timestamp, value)' pair in 'key' bucket.
+        keyTimeMap[key][timestamp] = value;
     }
     
     string get(string key, int timestamp) {
-        if(mp.find(key) == mp.end()) return "";
-        for(int i =timestamp;i>=1;--i){
-            if(mp[key].find(i) != mp[key].end()){
-                return mp[key][i];
+        // If the 'key' does not exist in map we will return empty string.
+        if (!keyTimeMap.count(key)) {
+            return "";
+        }
+        
+        // Iterate on time from 'timestamp' to '1'.
+        for (int currTime = timestamp; currTime >= 1; --currTime) {
+            // If a value for current time is stored in key's bucket we return the value.
+            if (keyTimeMap[key].count(currTime)) {
+                return keyTimeMap[key][currTime];
             }
         }
+        
+        // Otherwise no time <= timestamp was stored in key's bucket.
         return "";
     }
 };
-
-/**
- * Your TimeMap object will be instantiated and called as such:
- * TimeMap* obj = new TimeMap();
- * obj->set(key,value,timestamp);
- * string param_2 = obj->get(key,timestamp);
- */
