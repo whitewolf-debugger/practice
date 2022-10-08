51bcbe04a9e975246c8826a47625102e3b371f5e
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-10-08 09:33:32 +0530

    Time: 642 ms (23.18%), Space: 16.4 MB (48.38%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -24,3 +24,18 @@ public:
        return ans;
     }
 };
+/*
+Initialize the minimum difference diff with a large value.
+Sort the input array nums.
+Iterate through the array:
+For the current position i, set lo to i + 1, and hi to the last index.
+While the lo pointer is smaller than hi:
+Set sum to nums[i] + nums[lo] + nums[hi].
+If the absolute difference between sum and target is smaller than the absolute value of diff:
+Set diff to target - sum.
+If sum is less than target, increment lo.
+Else, decrement hi.
+If diff is zero, break from the loop.
+Return the value of the closest triplet, which is target - diff.
+
+*/
