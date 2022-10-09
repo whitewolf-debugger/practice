2199e320a9681b644ecbaf7772d64fbbc8681f69
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-10-09 22:42:46 +0530

    Time: 119 ms (7.71%), Space: 36.1 MB (94.70%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -11,38 +11,23 @@
  */
 class Solution {
 public:
+    TreeNode *find(TreeNode* root,int target){
+        if(!root) return NULL;
+        if(root -> val == target) return root;;
+        TreeNode* left = find(root -> left,target);
+        TreeNode* right = find(root -> right,target);
+        return left == NULL ? right : left;
+    }
+    bool helper(TreeNode* originalroot,TreeNode* root,int k){
+        if(!root) return false;
+        int target = k - root -> val;
+        TreeNode* searched = find(originalroot,target);
+        if(searched != root && searched != NULL) return true;
+        bool left = helper(originalroot,root->left,k);
+        bool right = helper(originalroot,root->right,k);
+        return left || right;
+    }
     bool findTarget(TreeNode* root, int k) {
-        unordered_set<int> st;
-        vector<int> v;
-        queue<TreeNode*> pendingNodes;
-        pendingNodes.push(root);
-        while(!pendingNodes.empty()){
-            TreeNode * front  = pendingNodes.front();
-            pendingNodes.pop();
-            //insert the elements of the tree in the vector and set 
-            st.insert(front->val);
-            v.push_back(front->val);
-            //bfs
-            if(front->left!=NULL){
-                pendingNodes.push(front->left);
-            }
-            if(front->right!=NULL){
-                pendingNodes.push(front->right);
-            }
-        }
-        //search all the elements of tree if sum == k 
-        for(auto x: v){
-            int diff = k-x;
-            //erase x so that we donot encounter error in case [1] sum =2 where 1+1 can be 2
-            st.erase(x);
-            if(st.find(diff)!=st.end()){
-                //return true if we got the diff and x is there in the set
-                return true;
-            }
-            //re insert that element 
-            st.insert(x);
-        }
-        //ans is false otherwise 
-        return false;
+        return helper(root,root,k);
     }
 };
