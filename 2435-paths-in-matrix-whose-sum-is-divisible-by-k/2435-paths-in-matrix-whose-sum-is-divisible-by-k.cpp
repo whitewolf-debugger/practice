5bacbe0622e91d25013589565d37174844711b61
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-10-09 10:00:36 +0530

    Time: 1274 ms (22.22%), Space: 139.4 MB (33.33%) - LeetHub

---
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long mod = 1e9+7;
+    int dp(vector<vector<int>> &grid,vector<vector<bool>> &visited,int r,int c,int ROWS,int COLS,long long sum,int k,vector<vector<vector<int>>>& cache){
+        if(r >= ROWS || c >= COLS) return 0;
+        if(r == ROWS-1 && c== COLS-1){
+            sum += grid[r][c]%mod;
+            if(sum % k ==0){
+                return 1;
+            }
+            return 0;
+        }
+        if(cache[r][c][sum%k] != -1) return cache[r][c][sum%k];
+        int right = dp(grid,visited,r+1,c,ROWS,COLS,sum+grid[r][c],k,cache)%mod;
+        int down = dp(grid,visited,r,c+1,ROWS,COLS,sum+grid[r][c],k,cache)%mod;
+        return cache[r][c][sum%k] = (right + down)%mod;
+
+    }
+    int numberOfPaths(vector<vector<int>>& grid, int k) {
+        long long ans = 0;
+        int ROWS =  grid.size();
+        int COLS = grid[0].size();
+        vector<vector<vector<int>>> cache(ROWS,vector<vector<int>>(COLS,vector<int>(k,-1)));
+        vector<vector<bool>> visited(ROWS,vector<bool>(COLS,false));
+        ans = dp(grid,visited,0,0,ROWS,COLS,0,k,cache);
+        return ans;
+    }
+};
