432d852ee78930537130cddf97bedeb4e327ae3a
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-10-11 18:53:20 +0530

    Time: 127 ms (51.55%), Space: 61.5 MB (67.34%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -3,18 +3,19 @@ public:
     bool increasingTriplet(vector<int>& nums) {
         int n = nums.size();
         bool Left = false, Right = false , Middle = false;
-        int l = INT_MAX;
-        int r = INT_MAX;
-        int mid = INT_MAX;
+        int l = INT_MAX ,  r = INT_MAX , mid = INT_MAX;
         for(int i =0; i < n;i++) {
+            // we want the left pinter to be placed at the minimum element 
             if(l > nums[i]) {
                 l  = nums[i];
                 Left = true;
             }
+            //we want the middle pointer to be placed at the element greater than left element 
             if(nums[i] > l && nums[i] < mid) {
                 mid= nums[i];
                 Middle = true;
             }
+            //we want the right to be placed at the poisition of element greater than the mid element 
             if(nums[i] > mid && nums[i] <= r){
                 r = nums[i];
                 Right = true;
