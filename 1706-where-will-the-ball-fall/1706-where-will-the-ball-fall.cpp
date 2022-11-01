d3bcff2a5cbb91b2d3cff4997fca68cef9bdca94
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-11-01 17:01:19 +0530

    Time: 26 ms (92.31%), Space: 13.1 MB (80.92%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int helper(vector<vector<int>> &grid,int row,int col,int ROWS,int COLS) {
+        //base case of the current row exceeds the max row size then the ball has escaped so we return the column 
         if(row >= ROWS) return col;
+        //get the next Column 
         int nextColumn = col + grid[row][col];
+        //if nextColumn is exceeding the walls means the ball is trapped 
         if(nextColumn < 0 || nextColumn >= COLS) return -1;
-        if(grid[row][col] != grid[row][nextColumn]) return -1;
+        //if there is a v shape where grid[row][col] = 1 and grid[row][nextColumn] = -1
+        if((grid[row][col] ==1 &&  grid[row][nextColumn] == -1) || (grid[row][col] == -1 &&  grid[row][nextColumn] == 1))return -1;
         return helper(grid,row+1,nextColumn,ROWS,COLS);
     }
     vector<int> findBall(vector<vector<int>>& grid) {
+        //row size 
         int ROWS = grid.size();
+        //column size 
         int COLS = grid[0].size();
+        //result array
         vector<int> ans(COLS);
+        //run a loop from 1st column to the last
         for(int i =0 ; i < COLS; i++) {
             ans[i] = helper(grid,0,i,ROWS,COLS);
         }
