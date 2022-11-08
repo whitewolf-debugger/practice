fd7924070707e36bd74ec9da6042ef3d4f6cc626
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-11-08 11:57:49 +0530

    Time: 5 ms (54.29%), Space: 6.7 MB (26.42%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> st;
+        for(int i =0; i <s.size();i++) {
+            if(isupper(s[i])) {
+                if(st.empty()) {
+                    st.push(s[i]);
+                } else {
+                    char c = st.top();
+                    if(islower(c) && toupper(c) == s[i]) {
+                        st.pop();
+                    } else {
+                        st.push(s[i]);
+                    }
+                }
+            } else {
+                if(st.empty()) {
+                    st.push(s[i]);
+                } else {
+                    char c = st.top();
+                    if(isupper(c) && tolower(c) == s[i]) {
+                        st.pop();
+                    } else {
+                        st.push(s[i]);
+                    }
+                }
+            }
+        }
+        string ans;
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
