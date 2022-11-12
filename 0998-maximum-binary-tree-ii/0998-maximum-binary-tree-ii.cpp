f77610ae94aa5ce16696854f6e12ce1bd5534fb1
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-11-12 22:43:34 +0530

    Time: 11 ms (43.97%), Space: 13.6 MB (19.87%) - LeetHub

--- 0998-maximum-binary-tree-ii/0998-maximum-binary-tree-ii.cpp
+++ 0998-maximum-binary-tree-ii/0998-maximum-binary-tree-ii.cpp
@@ -12,17 +12,21 @@
 class Solution {
 public:
     TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
+        // if root is NULL then we create a newNode and return 
         if(root == NULL) {
             TreeNode* node = new TreeNode(val);
             return node;
         }
+        //if value at root is less then we create a new node and attatch the root to its left
         else if(root -> val < val) {
             TreeNode* node = new TreeNode(val);
             node -> left = root;
             return node;
         } else {
+            //other wise if roots value is bigger then we call to roots right for lower values 
             root -> right = insertIntoMaxTree(root -> right , val);
         }
+        //return root after insertion
         return root;
     }
 };
