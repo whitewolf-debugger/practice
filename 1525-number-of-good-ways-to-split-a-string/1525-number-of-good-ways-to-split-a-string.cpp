c70a3765e3118d84ddeea81a8190369144a419a5
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-11-12 17:44:35 +0530

    Time: 97 ms (19.77%), Space: 8.4 MB (82.81%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //counts number of distinct characters in the map 
     int countDistinct(vector<int> &freqMap) {
         int count = 0 ;
         int distinct = 0;
@@ -11,20 +12,27 @@ public:
         return distinct;
     }
     int numSplits(string s) {
+        //keep 2 maps 
         vector<int> um(26,0);
         vector<int> mp(26,0);
+        //first map intially has the frequency of all the characters 
         for(int i =0 ; i < s.size();i++) {
             um[s[i] - 'a']++;
         }
+        //intialize ans to be 0 at first
         int ans = 0;
+        //increase the frequncy in new map with word till 0 to i (mp) and decrease freq of same in (um)
         for(int i =0 ; i < s.size();i++) {
+            //doing this we have now map of 2 half 0 to i in mp and i + 1 to end in um 
             mp[s[i] - 'a']++;
             um[s[i] - 'a']--;
+            //check whether number of distinct letters in 2 halves are equal or not increase ans 
             if(countDistinct(um) ==countDistinct(mp)) {
                 ans++;
             }
             
         }
+        //return ans 
         return ans;
     }
 };
