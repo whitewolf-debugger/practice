0a8464544d30a06fd778a77d10fa90861dc2b8a8
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-11-13 18:15:27 +0530

    Time: 998 ms (50.00%), Space: 155.8 MB (100.00%) - LeetHub

--- 2467-most-profitable-path-in-a-tree/2467-most-profitable-path-in-a-tree.cpp
+++ 2467-most-profitable-path-in-a-tree/2467-most-profitable-path-in-a-tree.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
+    //calculates the maximum profit path for alice 
     int calProfit(vector<vector<int>>& adj,int startVertex,int parent,vector<int> &amount) {
+        //get the current profit irrespective of whether positive or negetive we need to add it to check for max profit path
         int currProfit = amount[startVertex];
         int ans = INT_MIN;
+        //do dfs and ans is the maximum profit after doing dfs 
         for(auto &nbr : adj[startVertex]) {
+            //if the neighbour is the current node itself or the parent node then we skip rest since it will create self loop
             if(nbr == startVertex || nbr == parent) continue;
             ans = max(ans , calProfit(adj,nbr,startVertex,amount));
         }
+        // base case if ans is INT_MIN means we have reached the leaf node then we return the profit amount at that node 
         if(ans == INT_MIN) return currProfit;
+        //else we add the current profit to the ans and return it 
         else return currProfit + ans;
     }
+    //do dfs and update parent of everynode along with distance 
     void dfs(int startVertex , int p, int d ,vector<vector<int>> &adj,vector<int> &distance , vector<int> &parent) {
         distance[startVertex] = min(distance[startVertex] ,d);
         parent[startVertex] = p;
         for(auto &nbr : adj[startVertex]) {
+            //check for valid neighbour to avoid self loop 
             if(nbr == startVertex || nbr == p) continue;
             dfs(nbr,startVertex,d+1,adj,distance,parent);
         }
@@ -26,11 +34,16 @@ public:
             adj[edge[0]].push_back(edge[1]);
             adj[edge[1]].push_back(edge[0]);
         }
+        //set the distance to max for every node since we want minimum distance for every node for alice in distance vector
         vector<int> distance(n,INT_MAX);
+        //parent stores the parent so that we can track path to 0 for bob 
         vector<int> parent(n);
         dfs(0,0,0,adj,distance,parent);
+        //initialdistance of bob is 0 ofc he is at his starting place 
         int bobDistance = 0;
+        //set the current bobpostion to bob 
         int currBobPosition = bob;
+        //we search untill currBobPosition is not 0 means bob has not reached 0 
         while(currBobPosition != 0) {
             //if the distance of node from alice is more than the distance from bob and it is in path to 0 then bob will definately cover it 
             if(bobDistance < distance[currBobPosition]) {
@@ -47,7 +60,7 @@ public:
             //increase the distance of bob as we move to the next node 
             bobDistance++;
         }
-        vector<bool> visited(n,false);
+        //ans stores the ans 
         int ans = calProfit(adj,0,0,amount);
         return ans;
     }
