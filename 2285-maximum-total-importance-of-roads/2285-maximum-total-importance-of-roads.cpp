8c7757540167decae91a8b1ef672c03a96335817
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-11-13 22:58:49 +0530

    Time: 906 ms (46.86%), Space: 114 MB (71.91%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
         vector<long long> connections(n);
-       for(auto & edge: roads) {
+        for(auto & edge: roads) {
            connections[edge[0]]++;
            connections[edge[1]]++;
-       }
+        }
         long long ans = 0;
         sort(connections.rbegin() , connections.rend());
         for(int i =0 ; i < connections.size();i++) {
