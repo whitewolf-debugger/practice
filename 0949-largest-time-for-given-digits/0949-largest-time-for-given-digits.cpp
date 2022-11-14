1baee29f859d282cc086186d4ee7375189750370
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-11-15 00:53:52 +0530

    Time: 107 ms (5.26%), Space: 15.2 MB (5.26%) - LeetHub

---
+++ 0949-largest-time-for-given-digits/0949-largest-time-for-given-digits.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPossible(vector<int> &arr,int hour ,int minutes) {
+        vector<int> time(4);
+        time[0] = hour/10;
+        time[1] = hour % 10;
+        time[2] = minutes / 10;
+        time[3] = minutes % 10;
+        sort(arr.begin(),arr.end());
+        sort(time.begin(),time.end());
+        return time == arr;     
+    }
+    string largestTimeFromDigits(vector<int>& arr) {
+        string chonk = "";
+        for(int hour = 23; hour >=0 && chonk == "";hour--) {
+            for(int minutes = 59; minutes >=0 && chonk == "" ; minutes--) {
+                if(isPossible(arr,hour,minutes)) {
+                    chonk.push_back('0' + (hour/10)) ;
+                    chonk.push_back('0' + (hour % 10)) ;
+                    chonk.push_back(':') ;
+                    chonk.push_back('0' + (minutes / 10)) ;
+                    chonk.push_back('0' + (minutes % 10)) ;
+                    break;
+                }
+            }
+        }
+        return chonk;
+    }
+};
