a49c8bf9ca6c8b822769a56eb68d06a20389e244
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-11-16 20:08:35 +0530

    Time: 856 ms (21.53%), Space: 10.2 MB (73.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,8 +5,7 @@ public:
         for(int i =0;i<nums.size();i++){
             for(int j =i+1;j<nums.size();j++){
                 if(nums[i]+nums[j]==target){
-                    ans.push_back(i);
-                    ans.push_back(j);
+                    ans = {i,j};
                 }
             }
         }
