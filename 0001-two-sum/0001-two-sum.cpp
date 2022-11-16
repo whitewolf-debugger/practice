778d3ca4c7fcb75373e92f1c293d739497748a70
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-11-16 20:06:48 +0530

    Time: 607 ms (32.48%), Space: 10 MB (96.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,35 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-         unordered_map<int,vector<int>> mp;
-        for(int i =0; i < nums.size();i++) {
-            mp[nums[i]].push_back(i);
-        }
-        unordered_set<int> skip;
         vector<int> ans;
-        for(int i =0; i < nums.size();i++) {
-            if(skip.find(i) != skip.end()) continue;
-            int diff = target - nums[i];
-            if( diff == nums[i]) {
-                if(mp[nums[i]].size() >=2) {
-                   
-                    int count = 0;
-                    while(count < 2){
-                        auto it = mp[nums[i]].begin();
-                        ans.push_back(*it);
-                        mp[nums[i]].erase(it);
-                        count++;
-                        skip.insert(*it);
-                    }
-                }
-            } else {
-                if(mp[diff].size() > 0) {
-                    auto it = mp[diff].begin();
+        for(int i =0;i<nums.size();i++){
+            for(int j =i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
                     ans.push_back(i);
-                    ans.push_back(*it);
-                    skip.insert(*it);
-                    mp[diff].erase(it);
-                    
+                    ans.push_back(j);
                 }
             }
         }
