785948cfef810eadda7a1bc59ee8ac5f51a108e6
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-11-17 00:07:06 +0530

    Time: 1523 ms (23.18%), Space: 147.4 MB (17.38%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -2,12 +2,15 @@ class Solution {
 public:
     bool hasAllCodes(string s, int k) {
         if(s.size()<k) return false;
-        unordered_set<string> combs;
-        for(int i = 0 ; i <= s.size()-k;i++){
-            if(combs.find(s.substr(i,k))==combs.end()){
-                combs.insert(s.substr(i,k));
-            } 
+        unordered_set<string> st;
+        for(int i =0 ; i <= s.size() - k ; i++) {
+            if(st.find(s.substr(i,k)) == st.end()) st.insert(s.substr(i,k));
         }
-        return (combs.size()==pow(2,k));
+        return st.size() == pow(2,k);
     }
 };
+/*
+    
+
+
+*/
