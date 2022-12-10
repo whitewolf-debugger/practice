1dcfd3ee8c92e4826845215afe7a49b0a7569209
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-12-10 19:16:16 +0530

    Time: 213 ms (74.58%), Space: 78.1 MB (55.10%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -12,6 +12,8 @@
 class Solution {
 public:
     int MOD = 1e9 + 7;
+    
+    //gets the sum of the tree
     int subtreeSum(TreeNode * root) {
         if(!root) return 0;
         int leftSum = subtreeSum(root -> left);
@@ -19,13 +21,16 @@ public:
         return root -> val + leftSum + rightSum;
     }
     
+    //gets the ans where ans is passed as reference 
     long long getAns(TreeNode* root , long long &totalSum ,long long &ans) {
         if(!root) return 0;
         long long left = getAns(root -> left, totalSum, ans);
         long long right = getAns(root -> right, totalSum ,ans);
-
+        //get the sum of current subree 
         long long currHalf = (root -> val + left + right) % MOD;
+        //sum of remaining subtree is total - curr 
         long long remainingHalf = (totalSum - currHalf) % MOD;
+        //get the ans 
         ans = max(ans , (currHalf * remainingHalf));
         return currHalf;
     }
@@ -35,6 +40,7 @@ public:
 
         long long ans = 0;
         getAns(root , totalSum,ans);
+        //that you need to maximize the answer before taking the mod and not after taking it.
         return ans % MOD;
 
     }
