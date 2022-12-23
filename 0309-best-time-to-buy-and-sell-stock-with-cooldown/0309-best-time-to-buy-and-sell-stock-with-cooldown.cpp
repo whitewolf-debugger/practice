e1c79ac06fde07f6e9af9ffdfbdd8ac50081463a
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-12-23 13:31:20 +0530

    Time: 4 ms (80.62%), Space: 11.8 MB (46.95%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,37 +1,56 @@
 class Solution {
 public:
-    int dp(vector<int> prices, int i,bool buy,vector<vector<int>> &cache) {
-        if(i >= prices.size()) return 0;
-        int profit = 0;
-        if(cache[i][buy] != -1) return cache[i][buy];
-        if(buy) {
-            //option 1 buy the stocks
-            int op1 = -prices[i] + dp(prices, i+1, false,cache);
-            
-            //option 2 donot buy it
-            int op2 =  dp(prices, i+1, buy,cache);
-            profit = max(op1, op2);
-            
-        } else {
-            //sell it 
-            int op1 = prices[i] + dp(prices, i+2, true,cache);
-            
-            
-            //donot sell it
-            int op2 =  dp(prices, i+1, buy,cache);
-            
-            profit = max(op1, op2);
-        }
-        
-        return cache[i][buy] = profit;
-    }
-    
-    
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        int profit = 0;
-        vector<vector<int>> cache(n , vector<int>(2, -1));
-        profit = dp(prices, 0,true,cache);
-        return profit;
+        vector<vector<int>> dp(n + 2,vector<int>(2, 0));
+        
+        for(int i = n-1; i >=0 ;i--) {
+            for(int buy = 0; buy <= 1; buy++) {
+                if(buy) {
+                    dp[i][buy] = max(-prices[i] + dp[i + 1][0] ,0 + dp[i+1][1]);
+                } else {
+                    dp[i][buy] = max(prices[i] + dp[i + 2][1] ,0 + dp[i+1][0]);
+                }
+            }
+        }
+        
+        return dp[0][1];
     }
+
 };
+
+
+//     int dp(vector<int> prices, int i,bool buy,vector<vector<int>> &cache) {
+//         if(i >= prices.size()) return 0;
+//         int profit = 0;
+//         if(cache[i][buy] != -1) return cache[i][buy];
+//         if(buy) {
+//             //option 1 buy the stocks
+//             int op1 = -prices[i] + dp(prices, i+1, false,cache);
+            
+//             //option 2 donot buy it
+//             int op2 =  dp(prices, i+1, buy,cache);
+//             profit = max(op1, op2);
+            
+//         } else {
+//             //sell it 
+//             int op1 = prices[i] + dp(prices, i+2, true,cache);
+            
+            
+//             //donot sell it
+//             int op2 =  dp(prices, i+1, buy,cache);
+            
+//             profit = max(op1, op2);
+//         }
+        
+//         return cache[i][buy] = profit;
+//     }
+    
+    
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         int profit = 0;
+//         vector<vector<int>> cache(n , vector<int>(2, -1));
+//         profit = dp(prices, 0,true,cache);
+//         return profit;
+//     }
