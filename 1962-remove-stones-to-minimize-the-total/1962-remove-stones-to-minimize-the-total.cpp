df7aac66871bdae4ea474cb8ec64c68a3d8b4295
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2022-12-28 12:49:29 +0530

    Time: 566 ms (89.31%), Space: 105.1 MB (28.79%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -19,3 +19,14 @@ public:
         return sum - diff;
     }
 };
+/*
+
+    we have to remove  floor(piles[i] / 2) stones from piles k times
+    we want to return the minimum possible total number of stones remaining after applying the k operations.
+    minimum possile stones we can get by removing stones from maximum sized piles 
+    so we use priority queue to get maximum sized pile among the current piles and remove (piles[i] / 2) from that
+    we store the number of stones removed in diff
+    and ans is the sum without removing - stones removed 
+    
+
+*/
