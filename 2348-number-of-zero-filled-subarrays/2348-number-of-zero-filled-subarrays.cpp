8ceec2ba6825a4933ff5b7aed9dfe45885073375
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2023-03-21 18:05:42 +0530

    Time: 207 ms (9.20%), Space: 107.4 MB (97.44%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
+    //using sliding window approach we solve this question 
     long long zeroFilledSubarray(vector<int>& nums) {
+        //intially windowStart is at 0 
         int windowStart = 0;
         int n = nums.size();
         long long ans = 0;
+        //keep track of nonzero numbers encountered
         int nonZeroCount = 0;
         for(int windowEnd = 0; windowEnd < n; windowEnd++) {
             if(nums[windowEnd] != 0) {
