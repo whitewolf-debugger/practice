da1098d1575719cf21bfcb869f924ab4d1e766da
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2023-04-14 23:09:26 +0530

    Added solution - LeetHub

--- Remove the balls - GFG/remove-the-balls.cpp
+++ Remove the balls - GFG/remove-the-balls.cpp
@@ -14,7 +14,6 @@ class Solution {
         for(int i = 0 ; i < N; i++) {
             if(!st.empty() && st.top().first == color[i] && st.top().second == radius[i]) {
                 st.pop();
-                
             } 
             else{
                 st.push({color[i],radius[i]});
