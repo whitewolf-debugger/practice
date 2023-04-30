71b599bfd8b072d5c5decb26ed4063c2001648f1
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2023-05-01 01:19:08 +0530

    Added solution - LeetHub

---
+++ Subtree In N-ary Tree - GFG/subtree-in-nary-tree.cpp
@@ -0,0 +1,130 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    vector<Node*> children;
+    Node(int val){
+        data=val;
+    }
+};
+
+class N_ary_Tree{
+public:
+    Node *root;
+    int size;
+    N_ary_Tree(){
+        root=NULL;
+        size=0;
+    }
+    Node *add(int new_key,Node *parent_key=NULL){
+        Node *new_node = new Node(new_key);
+        if(parent_key == NULL){
+            root = new_node;
+            size = 1;
+        }
+        else{
+            parent_key->children.push_back(new_node);
+            size += 1;
+        }
+        return new_node;
+    }
+};
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    set<string> track;
+    
+    void dfs(Node *root, string &s) {
+        if (!root) return;
+        s += to_string(root->data) + ",";
+        for (int i = 0; i < root->children.size(); i++) {
+            if (root->children[i] != NULL) {
+                dfs(root->children[i], s);
+            }
+        }
+    }
+    
+    void helper(Node *root, set<string> &ans) {
+        if (!root) return;
+        queue<Node*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int size = q.size();
+            while (size--) {
+                Node* node = q.front();
+                string preorder = "";
+                dfs(node, preorder);
+                if (track.find(preorder) != track.end()) {
+                    ans.insert(preorder);
+                } else {
+                    track.insert(preorder);
+                }
+                q.pop();
+                for (int i = 0; i < node->children.size(); i++) {
+                    if (node->children[i] != NULL) {
+                        q.push(node->children[i]);
+                    }
+                } 
+            }
+        }
+    }
+    
+    int duplicateSubtreeNaryTree(Node *root) {
+        set<string> ans;
+        helper(root, ans);
+        return ans.size();
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        if(i==0){
+            string p;
+            getline(cin,p);
+        }
+        string str;
+        getline(cin,str);
+        stringstream st(str);
+        vector<string> s;
+        string y;
+        while(st>>y){
+            s.push_back(y);
+        }
+        N_ary_Tree *tree = new N_ary_Tree();
+        Node *curr;
+        queue<Node*> q;
+        for(int i=0;i<s.size();i++){
+            if (i == 0){
+                curr=tree->add(stoi(s[0]));
+            }
+            else if(s[i] == " "){
+                continue;
+            }
+            else if(q.size() and s[i] == "N"){
+                curr = q.front();
+                q.pop();
+            }
+            else if(s[i] != "N"){
+                q.push(tree->add(stoi(s[i]), curr));
+            }
+        }
+        Solution ob;
+        int res = ob.duplicateSubtreeNaryTree(tree->root);
+        cout<<res<<endl;
+    }
+}
+// } Driver Code Ends
